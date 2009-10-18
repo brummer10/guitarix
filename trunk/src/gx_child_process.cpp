117c647d1f4c46ff867f0db49860161c563971ad
hermann <brummer-@web.de>
2009-10-18 03:38:13 +0000

    fix for startup with jconv enabled

--- trunk/src/gx_child_process.cpp
+++ trunk/src/gx_child_process.cpp
@@ -478,7 +478,7 @@ namespace gx_child_process
 	}
       }
 
-      else
+      else if(gx_engine::initialized == true)
       {
 	// check whether jconv is installed in PATH
 	int const jconv_ok = gx_system_call("which", "jconv");
