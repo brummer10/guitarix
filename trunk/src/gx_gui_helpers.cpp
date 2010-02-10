42e20092c3ba20e72f56fbd9a8ab889bec050662
hermann <brummer-@web.de>
2010-02-10 19:01:19 +0000

    set timeout for Xrun report

--- trunk/src/gx_gui_helpers.cpp
+++ trunk/src/gx_gui_helpers.cpp
@@ -239,7 +239,9 @@ namespace gx_gui
 
     void gx_jack_report_xrun()
     {
-      g_idle_add(gx_threads::gx_xrun_report,gpointer (NULL));
+      guint xrun_id = 0;
+      if (xrun_id == 0 ||g_main_context_find_source_by_id(NULL, xrun_id) == NULL)
+                xrun_id =g_idle_add(gx_threads::gx_xrun_report,gpointer (NULL));
     }
 
     //----menu function gx_show_oscilloscope
