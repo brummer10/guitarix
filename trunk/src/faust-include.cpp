64e82d4a7c2a3b5d21602438788e7f5b816d1986
Andreas Degert <andreas.degert@googlemail.com>
2010-05-29 12:49:46 +0000

    make --experimental work with -DNDEBUG (--debug not set)

--- trunk/src/faust-include.cpp
+++ trunk/src/faust-include.cpp
@@ -478,7 +478,8 @@ GTKUI::GTKUI(GtkWidget *window)
 
 void GTKUI::pushBox(int mode, GtkWidget* w)
 {
-    assert(++fTop < stackSize);
+	++fTop;
+    assert(fTop < stackSize);
     fMode[fTop]     = mode;
     fBox[fTop]      = w;
 }
@@ -501,7 +502,8 @@ void GTKUI::adjustStack(int n)
 
 void GTKUI::closeBox()
 {
-    assert(--fTop >= 0);
+	--fTop;
+    assert(fTop >= 0);
 }
 
 
