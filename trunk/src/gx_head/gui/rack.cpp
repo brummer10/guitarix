brummer10/guitarix

commit 437c2371ced55e8d8cf1e3dc1f7bf9cf3c732912

DnD: make the new "first make space, then show RackBox" code really work.
RackBox::swtch must be used earlier because it contains an
unconditional show().

trunk/src/gx_head/gui/rack.cpp
@@ -118,11 +118,11 @@ void PluginUI::display(bool v, bool animate) {
 }
 
 void PluginUI::display_new(bool unordered) {
+    rackbox->swtch(false);
     display(true, true);
     if (!unordered) {
 	rackbox->get_parent()->reorder(get_id(), -1);
     }
-    rackbox->swtch(false);
 }
 
 /****************************************************************

commit 7862c61e60a7836ebc140d07e1e31dbade96ef09

Fixed segfault introduced with commit 437c2371

trunk/src/gx_head/gui/rack.cpp
@@ -118,7 +118,10 @@ void PluginUI::display(bool v, bool animate) {
 }
 
 void PluginUI::display_new(bool unordered) {
-    rackbox->swtch(false);
+    if (rackbox) {
+	rackbox->swtch(false);
+    }
+    compressed = false;
     display(true, true);
     if (!unordered) {
 	rackbox->get_parent()->reorder(get_id(), -1);