brummer10/guitarix

commit 7b3e1c9ca342c26ccbd2780e5ad11f793a31aeb1

GxRegler: just hide the popup display, don't destroy it
that's the only fix I can come up with at the moment for funny things
happening in the atk (libgail, producing warning messages about
invalid objects).

made the popup work a little smoother

remove unused variables and some other small fixes

trunk/libgxw/gxw/GxRegler.cpp
@@ -308,8 +308,6 @@ static void gx_regler_class_init(GxReglerClass *klass)
 	GtkWidgetClass *widget_class = (GtkWidgetClass*) klass;
 	GtkBindingSet  *binding_set;
 
-	klass->change_value_id = g_signal_lookup("change-value", GTK_TYPE_RANGE);
-
 	gobject_class->finalize = gx_regler_finalize;
 	gobject_class->set_property = gx_regler_set_property;
 	gobject_class->get_property = gx_regler_get_property;
@@ -1087,11 +1085,20 @@ static void gx_regler_set_value (GtkWidget *widget, int dir_down)
  ** mouse button pressed set value
  */
 
+static void hide_dialog(GtkWidget *dialog)
+{
+	gdk_keyboard_ungrab(GDK_CURRENT_TIME);
+	GdkWindow *window = gtk_widget_get_window(dialog);
+	gdk_display_pointer_ungrab(gdk_drawable_get_display(window), GDK_CURRENT_TIME);
+	gtk_grab_remove(dialog);
+	gtk_widget_hide(dialog);
+}
+
 static gboolean dialog_button_press_event(
 	GtkWidget *widget, GdkEventButton *event, gpointer data)
 {
-	if (event->type == GDK_BUTTON_PRESS) {
-		gtk_widget_destroy(GTK_WIDGET(data));
+	if (event->type == GDK_BUTTON_PRESS || event->type == GDK_2BUTTON_PRESS) {
+		hide_dialog(GTK_WIDGET(data));
 	}
 	return TRUE;
 }
@@ -1111,8 +1118,9 @@ static gboolean dialog_key_press_event(
 	if (event->is_modifier) {
 		return FALSE;
 	}
-	if (GTK_IS_WIDGET(data));
-		gtk_widget_destroy(GTK_WIDGET(data));
+	if (GTK_IS_WIDGET(data)) {
+		hide_dialog(GTK_WIDGET(data));
+	}
 	return FALSE;
 }
 
@@ -1122,8 +1130,9 @@ static gboolean dialog_key_press_before(
 	if (event->keyval == GDK_Escape) {
 		// spinbutton to current adjustment value
 		gtk_adjustment_value_changed(GTK_SPIN_BUTTON(widget)->adjustment);
-		if (GTK_IS_WIDGET(data));
-			gtk_widget_destroy(GTK_WIDGET(data));
+		if (GTK_IS_WIDGET(data)) {
+			hide_dialog(GTK_WIDGET(data));
+		}
 		return TRUE;
 	}
 	return FALSE;
@@ -1132,8 +1141,9 @@ static gboolean dialog_key_press_before(
 static gboolean dialog_grab_broken(
 	GtkWidget *widget, GdkEvent *event, gpointer data)
 {
-	if (GTK_IS_WIDGET(data));
-		gtk_widget_destroy(GTK_WIDGET(data));
+	if (GTK_IS_WIDGET(data)) {
+		hide_dialog(GTK_WIDGET(data));
+	}
 	return FALSE;
 }
 
@@ -1150,14 +1160,14 @@ static gboolean map_check(
 	                      NULL, c, GDK_CURRENT_TIME);
 	gdk_cursor_unref(c);
 	if (rc != GDK_GRAB_SUCCESS) {
-		gtk_widget_destroy(dialog);
+		hide_dialog(dialog);
 		return TRUE;
 	}
 	rc = gdk_keyboard_grab(window, TRUE, GDK_CURRENT_TIME);
 	if (rc != GDK_GRAB_SUCCESS) {
 		gdk_display_pointer_ungrab(gdk_drawable_get_display(window),
 		                           GDK_CURRENT_TIME);
-		gtk_widget_destroy(dialog);
+		hide_dialog(dialog);
 		return TRUE;
 	}
 	gtk_grab_add(dialog);
@@ -1166,32 +1176,41 @@ static gboolean map_check(
 
 static gboolean gx_regler_value_entry(GxRegler *regler, GdkRectangle *rect, GdkEventButton *event)
 {
+	if (event->type == GDK_2BUTTON_PRESS || event->type == GDK_3BUTTON_PRESS) {
+		return FALSE;
+	}
 	g_assert(GX_IS_REGLER(regler));
 	GtkAdjustment *adj = gtk_range_get_adjustment(GTK_RANGE(regler));
-	GtkWidget *dialog = gtk_window_new(GTK_WINDOW_POPUP);
-	gtk_widget_add_events(dialog, GDK_BUTTON_PRESS_MASK|GDK_BUTTON_MOTION_MASK);
 	ensure_digits(regler);
-	GtkWidget *spinner = gtk_spin_button_new(
-		GTK_ADJUSTMENT(adj), adj->step_increment,
-		GTK_RANGE(regler)->round_digits);
-	gtk_container_add (GTK_CONTAINER(dialog), spinner);
-	
-	g_signal_connect(spinner, "button-press-event", G_CALLBACK(spinner_button_press_event), NULL);
-	g_signal_connect(dialog, "button-press-event", G_CALLBACK(dialog_button_press_event), dialog);
-	g_signal_connect(spinner, "key-press-event", G_CALLBACK(dialog_key_press_before), dialog);
-	g_signal_connect_after(spinner, "key-press-event", G_CALLBACK(dialog_key_press_event), dialog);
-	g_signal_connect_object(spinner, "activate", G_CALLBACK(gtk_widget_destroy), dialog, (GConnectFlags)(G_CONNECT_AFTER|G_CONNECT_SWAPPED));
-	g_signal_connect(dialog, "grab-broken-event", G_CALLBACK(dialog_grab_broken), dialog);
-	g_signal_connect(dialog, "map-event", G_CALLBACK(map_check), GTK_WIDGET(regler));
-	
-	gtk_window_move(GTK_WINDOW(dialog), -100, -100); // trick so its not visible
-	gtk_widget_show_all(dialog);
+	static GtkWidget *dialog = 0;
+	static GtkWidget *spinner = 0;
+	if (!dialog) {
+		dialog = gtk_window_new(GTK_WINDOW_POPUP);
+		gtk_widget_add_events(dialog, GDK_BUTTON_PRESS_MASK|GDK_BUTTON_MOTION_MASK);
+		spinner = gtk_spin_button_new(
+			GTK_ADJUSTMENT(adj), adj->step_increment,
+			GTK_RANGE(regler)->round_digits);
+		gtk_widget_show(spinner);
+		gtk_container_add (GTK_CONTAINER(dialog), spinner);
+		g_signal_connect(spinner, "button-press-event", G_CALLBACK(spinner_button_press_event), NULL);
+		g_signal_connect(dialog, "button-press-event", G_CALLBACK(dialog_button_press_event), dialog);
+		g_signal_connect(spinner, "key-press-event", G_CALLBACK(dialog_key_press_before), dialog);
+		g_signal_connect_after(spinner, "key-press-event", G_CALLBACK(dialog_key_press_event), dialog);
+		g_signal_connect(spinner, "activate", G_CALLBACK(hide_dialog), dialog);
+		g_signal_connect(dialog, "grab-broken-event", G_CALLBACK(dialog_grab_broken), dialog);
+		g_signal_connect(dialog, "map-event", G_CALLBACK(map_check), GTK_WIDGET(regler));
+		gtk_widget_realize(GTK_WIDGET(dialog));
+	} else {
+		gtk_spin_button_configure(
+			GTK_SPIN_BUTTON(spinner), GTK_ADJUSTMENT(adj), adj->step_increment,
+			GTK_RANGE(regler)->round_digits);
+	}
 	GtkRequisition rq;
 	gtk_widget_get_requisition(dialog, &rq);
 	gint xorg, yorg;
 	gdk_window_get_origin(GTK_WIDGET(regler)->window, &xorg, &yorg);
-	
 	gtk_window_move(GTK_WINDOW(dialog), xorg+rect->x+(rect->width-rq.width)/2, yorg+rect->y+(rect->height-rq.height)/2);
+	gtk_widget_show(dialog);
 	return FALSE;
 }
 

trunk/libgxw/gxw/GxRegler.h
@@ -54,8 +54,6 @@ struct _GxRegler
 
 struct _GxReglerClass {
 	GtkRangeClass parent_class;
-	guint change_value_id;
-	gboolean (*values_entry)(GxRegler *regler, GdkRectangle *rect);
 	gboolean (*value_entry)(GxRegler *regler, GdkRectangle *rect, GdkEventButton *event);
 	gchar* (*format_value)(GxRegler *regler, gdouble value);
 };

commit 008d1e37a1c45bc3d8f375836634f5519cb17828

Fix error in value popup when using enter key
error introduced in commit 7b3e1c9c

trunk/libgxw/gxw/GxRegler.cpp
@@ -1196,7 +1196,7 @@ static gboolean gx_regler_value_entry(GxRegler *regler, GdkRectangle *rect, GdkE
 		g_signal_connect(dialog, "button-press-event", G_CALLBACK(dialog_button_press_event), dialog);
 		g_signal_connect(spinner, "key-press-event", G_CALLBACK(dialog_key_press_before), dialog);
 		g_signal_connect_after(spinner, "key-press-event", G_CALLBACK(dialog_key_press_event), dialog);
-		g_signal_connect(spinner, "activate", G_CALLBACK(hide_dialog), dialog);
+		g_signal_connect_swapped(spinner, "activate", G_CALLBACK(hide_dialog), dialog);
 		g_signal_connect(dialog, "grab-broken-event", G_CALLBACK(dialog_grab_broken), dialog);
 		g_signal_connect(dialog, "map-event", G_CALLBACK(map_check), GTK_WIDGET(regler));
 		gtk_widget_realize(GTK_WIDGET(dialog));