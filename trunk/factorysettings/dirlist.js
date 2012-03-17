edcf1400b78f1e658b30ba8ccc34b4d7b0a22c67
Andreas Degert <andreas.degert@googlemail.com>
2012-03-17 23:03:32 +0100

    renamed factory preset files from _rc to .gx
    the file uri is used when exporting a factory preset by DnD and should
    end with .gx to mark it as guitarix preset file.

--- trunk/factorysettings/dirlist.js
+++ trunk/factorysettings/dirlist.js
@@ -1,7 +1,7 @@
 [
-    ["funkmuscle",  "funkmuscle_rc"],
-    ["zettberlin",  "zettberlin_rc"],
-    ["autoandimat", "autoandimat_rc"],
-    ["StudioDave",  "dlp_ae_rc"],
-    ["JP",          "jp_n_o_s_rc"]
+    ["funkmuscle",  "funkmuscle.gx"],
+    ["zettberlin",  "zettberlin.gx"],
+    ["autoandimat", "autoandimat.gx"],
+    ["StudioDave",  "dlp_ae.gx"],
+    ["JP",          "jp_n_o_s.gx"]
 ]
