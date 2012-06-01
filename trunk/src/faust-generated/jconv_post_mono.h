e9eac2404516c0a18e18907f159be8f186c563a2
hermann <brummer-@web.de>
2012-06-01 18:37:29 +0200

    add forgotten file jconv_post_mono.h

---
+++ trunk/src/faust-generated/jconv_post_mono.h
@@ -0,0 +1,24 @@
+// generated from file '../src/faust/jconv_post_mono.dsp' by dsp2cc:
+// Code generated with Faust 0.9.43 (http://faust.grame.fr)
+
+#define FAUSTFLOAT float
+
+namespace jconv_post_mono {
+
+class Dsp {
+private:
+	int fSamplingFreq;
+	FAUSTFLOAT 	fslider0;
+	FAUSTFLOAT 	fslider1;
+	double 	fRec0[2];
+
+public:
+	void clear_state_f();
+	void init(unsigned int samplingFreq);
+	void compute(int count, float *input0, float *input1, float *output0);
+	int register_par(const ParamReg& reg);
+	Dsp();
+	~Dsp();
+};
+
+} // end namespace jconv_post_mono
