9c6e2c7ad755375134c4adc6a9024dee072b83ff
Andreas Degert <andreas.degert@googlemail.com>
2013-06-25 17:12:41 +0200

    call exit signal handler in reverse registration order
    The GxJack instance is created first and registers a handler,
    GxSettings comes second. Reverse order lets GxSettings write the jack
    connection information into the statefile before the jackd connection
    is shut down. This only applys to the exit signal handler, e.g. when
    stopping headless Guitarix with Ctrl-C.

--- trunk/src/gx_head/engine/gx_system.cpp
+++ trunk/src/gx_head/engine/gx_system.cpp
@@ -915,7 +915,7 @@ GxExit::GxExit(): exit_sig(), ui_thread() {}
 GxExit::~GxExit() {}
 
 void GxExit::exit_program(string msg, int errcode) {
-    exit_sig(pthread_equal(pthread_self(), ui_thread));
+    exit_sig.emit_reverse(pthread_equal(pthread_self(), ui_thread));
     if (msg.empty()) {
 	msg = "** guitarix exit **";
     }
