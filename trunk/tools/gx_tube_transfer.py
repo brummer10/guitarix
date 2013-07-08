eebd71fe5ef430936746bbbd17154b80ee6ceae4
hermann <brummer-@web.de>
2013-07-08 19:27:54 +0200

    * some tweaks to gx_tube_transfer.py

--- trunk/tools/gx_tube_transfer.py
+++ trunk/tools/gx_tube_transfer.py
@@ -19,9 +19,9 @@
 #
 
 
-import sys, os, gtk, StringIO, glib
+import sys, os, gtk, StringIO, glib, multiprocessing
 from pylab import *
-
+from time import sleep
 from tube_transfer import *
 
 class tube_transfer(gtk.Window):
@@ -29,10 +29,18 @@ class tube_transfer(gtk.Window):
     def on_exit(self, widget):
         self.destroy()
     
+    def on_clear(self, widget):
+        self.progressbar.hide()
+        self.image.hide()
+        self.textview.hide()
+        self.resize(550, 200)
+        
+    
     def set_tube(self):
         tubelist = sorted(tubes.keys())
         for tube in tubelist:
             self.tube.append_text(tube)
+    
     def get_param(self, c):
         self.table_size = c.table_size
         self.Ri_values = c.Ri_values
@@ -80,8 +88,31 @@ class tube_transfer(gtk.Window):
         output = os.linesep.join([s for s in output.splitlines() if s])
         self.labelaccuracy.set_text(output)
     
-    def on_plot(self, widget):
-        self.sw.set_size_request (-1, 480)
+    def on_plot_progress(self):
+        myfile="/tmp/tube.png"
+        self.progressbar.show()
+        if self.progressbar.get_fraction() >= 1.0:
+            value = 0.0
+        else:
+            value = self.progressbar.get_fraction() + 0.1
+        self.progressbar.set_fraction(value)
+        if os.path.isfile(myfile):
+            sleep(1)
+            self.sw.set_size_request (-1, 480)
+            self.image.set_from_file(myfile)
+            self.textview.hide()
+            self.image.show()
+            glib.idle_add(self.on_plot_finish)
+            self.progressbar.hide()
+            return False
+        return True
+
+    def run_plot_progress(self):
+        self.progressbar.set_text("plot %s" % self.tube.get_active_text())
+        self.progressbar.set_fraction(0.0)
+        glib.timeout_add(500, self.on_plot_progress)
+        
+    def on_plot_do(self):
         c = Circuit(self.tube.get_active_text(), self.func.get_active_text())
         self.set_param(c)
         myfile="/tmp/tube.png"
@@ -97,8 +128,14 @@ class tube_transfer(gtk.Window):
         axis
         savefig(myfile, dpi=80, bbox_inches=0)
         close()
-        self.image.set_from_file(myfile)
-        glib.idle_add(self.on_plot_finish)
+        
+    def on_plot(self, widget):
+        jobs = []
+        p = multiprocessing.Process(target=self.on_plot_do)
+        jobs.append(p)
+        p.start()
+        self.run_plot_progress()
+        
     def on_plot_finish(self):
         self.sw.set_size_request (-1, -1)
         
@@ -123,27 +160,70 @@ class tube_transfer(gtk.Window):
         output1 = capturer.getvalue().replace('\n','')
         self.labelvk01.set_text("Ri(%s) Rk(%s) vk0(%s) \nRi(%s) Rk(%s) vk0(%s)" % 
             (self.scale7.get_value(),self.scale5.get_value() ,output, self.scale6.get_value(),self.scale5.get_value() ,output1) )
-        
-    def on_table(self, widget, argv):
+    
+    def on_table_progress(self):
+        script_dir = sys.path[0]
+        save_path = os.path.join(script_dir, 'gxtubes')
+        myfile='%s/%s.cc' % (save_path,self.tube.get_active_text())
+        self.progressbar.show()
+        if self.progressbar.get_fraction() >= 1.0:
+            value = 0.0
+        else:
+            value = self.progressbar.get_fraction() + 0.1
+        self.progressbar.set_fraction(value)
+        if os.path.isfile(myfile):
+            sleep(1)
+            infile = open(myfile, "r")
+            if infile:
+                self.sw.set_size_request (-1, 480)
+                textbuffer = self.textview.get_buffer()
+                string = infile.read()
+                infile.close()
+                textbuffer.set_text(string)
+                self.image.hide()
+                self.textview.show()
+                glib.idle_add(self.on_plot_finish)
+            self.progressbar.hide()
+            
+            return False
+        return True
+    
+    def run_table_progress(self):
+        self.progressbar.set_text("write table %s" % self.tube.get_active_text())
+        self.progressbar.set_fraction(0.0)
+        glib.timeout_add(500, self.on_table_progress)
+
+    def on_table_do(self):
+        script_dir = sys.path[0]
+        save_path = os.path.join(script_dir, 'gxtubes')
+        myfile='%s/%s.cc' % (save_path,self.tube.get_active_text())
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        if os.path.isfile(myfile):
+            os.remove(myfile)
         c = Circuit(self.tube.get_active_text(), self.func.get_active_text())
         self.set_param(c)
         old_stdout = sys.stdout
         capturer = StringIO.StringIO()
         sys.stdout = capturer
-        c.write_tables(argv)
+        c.write_tables(self.arg)
         sys.stdout = old_stdout
         output = capturer.getvalue()
-        script_dir = sys.path[0]
-        save_path = os.path.join(script_dir, 'gxtubes')
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        
-        f = open('%s/%s.cc' % (save_path,self.tube.get_active_text()), 'w')
+        f = open(myfile, 'w')
         f.write(output)
         f.close
+
+    def on_table(self, widget):
+        jobs = []
+        p = multiprocessing.Process(target=self.on_table_do)
+        jobs.append(p)
+        p.start()
+        self.run_table_progress()
+    
     
     def __init__(self, argv):        
         super(tube_transfer, self).__init__()
+        self.arg = argv
         self.set_default_size(550, 200)
         self.set_border_width(10)
         self.connect("destroy", lambda w: gtk.main_quit())
@@ -151,9 +231,11 @@ class tube_transfer(gtk.Window):
         vbox = gtk.VBox(False, 10)
         hbox = gtk.HBox(False, 8)
         hbox1 = gtk.HBox(False, 8)
+        vbox1 = gtk.VBox(False, 10)
         self.sw = gtk.ScrolledWindow()
         self.sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
         self.sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.progressbar = gtk.ProgressBar()
         table = gtk.Table(2,9,False)
         table.set_col_spacings(5)
         label = gtk.Label("Tube")
@@ -167,8 +249,13 @@ class tube_transfer(gtk.Window):
         self.labelvk01.set_selectable(True)
         self.labeltv = gtk.Label("")
         hbox1.pack_end(self.labeltv, False, False, 0)
+        hbox1.pack_start(self.progressbar, False, False, 0)
+        self.progressbar.hide()
         self.image = gtk.Image()
-        self.sw.add_with_viewport(self.image)
+        self.textview = gtk.TextView()
+        vbox1.pack_start(self.image, False, False, 0)
+        vbox1.pack_start(self.textview, False, False, 0)
+        self.sw.add_with_viewport(vbox1)
         
         label2 = gtk.Label("Rk (*e3)")
         self.scale5 = gtk.SpinButton()
@@ -222,7 +309,7 @@ class tube_transfer(gtk.Window):
         fun = gtk.Button("plot")
         fun.connect("clicked", self.on_plot)
         fun1 = gtk.Button("table")
-        fun1.connect("clicked", self.on_table, argv)
+        fun1.connect("clicked", self.on_table)
         fun2 = gtk.Button("vk0")
         fun2.connect("clicked", self.on_vk0)
         self.labelaccuracy = gtk.Label("")
@@ -230,6 +317,8 @@ class tube_transfer(gtk.Window):
         fun3.connect("clicked", self.on_accuracy)
         fun4 = gtk.Button("exit")
         fun4.connect("clicked", self.on_exit)
+        fun5 = gtk.Button("clear")
+        fun5.connect("clicked", self.on_clear)
         
         self.tube.connect("changed", self.on_tube_changed)
         self.tube.set_active(2)
@@ -260,6 +349,7 @@ class tube_transfer(gtk.Window):
         vbox.pack_end(hbox, False, False, 0)
         
         hbox.pack_end(fun4, False, False, 0)
+        hbox.pack_end(fun5, False, False, 0)
         hbox.pack_end(fun3, False, False, 0)
         hbox.pack_end(fun, False, False, 0)
         hbox.pack_end(fun1, False, False, 0)
@@ -268,7 +358,10 @@ class tube_transfer(gtk.Window):
         hbox.pack_start(self.labelaccuracy, False, False, 0)
         self.add(vbox)
         self.show_all()
-
+        self.progressbar.hide()
+        self.image.hide()
+        self.textview.hide()
+            
 def main():
     tube_transfer(sys.argv[0])
     gtk.main()
