brummer10/guitarix

commit 7dfdb325282fc1a3da1a171916b3a406eeb04690

selecteq.dsp: move pow() out of fast signal path
guitarix.lib: delete db2linear in eqfilter
selecteq.dsp: insert db2linear before smoothi, reformat

trunk/src/faust/guitarix.lib
@@ -274,12 +274,11 @@ eqfilter(Q,F,G)	= TF2(  (1 +  K/Q + K*K) 	/ D,
 						 2 * (K*K - 1) 		/ D,
 						(1 - K/Q + K*K) 	/ D,
 						 2 * (K*K - 1) 		/ D,
-						(1 - V*K/Q + K*K) 	/ D
+						(1 - G*K/Q + K*K) 	/ D
 					 )
 		with {
-				V = db2linear(G);
 				K = tan(PI*F/SR);
-				D = 1 + V*K/Q + K*K;
+				D = 1 + G*K/Q + K*K;
 		};
 
 

trunk/src/faust/selecteq.dsp
@@ -12,14 +12,53 @@ import("guitarix.lib");
 //------------------------- Process --------------------------------
 
 
-process =   ifilter(vslider("Qs31_25", 50, 1, 100, 1),   vslider("freq31_25 [tooltip:Hz]",31., 20, 20000, 1), vslider("fs31_25[tooltip:gain (dB) at 31.25 Hz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs62_5", 50, 1, 100, 1),   vslider("freq62_5 [tooltip:Hz]",62., 20, 20000, 1),  vslider("fs62_5 [tooltip:gain (dB) at 62.5 Hz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs125", 50, 1, 100, 1),  vslider("freq125 [tooltip:Hz]",125., 20, 20000, 1),    vslider("fs125  [tooltip:gain (dB) at 125 Hz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs250", 50, 1, 100, 1),  vslider("freq250 [tooltip:Hz]",250., 20, 20000, 1),    vslider("fs250  [tooltip:gain (dB) at 250 Hz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs500", 50, 1, 100, 1),  vslider("freq500 [tooltip:Hz]",500., 20, 20000, 1),    vslider("fs500  [tooltip:gain (dB) at 500 Hz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs1k", 50, 1, 100, 1), vslider("freq1k [tooltip:Hz]",1000., 20, 20000, 1),    vslider("fs1k   [tooltip:gain (dB) at 1 kHz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs2k", 50, 1, 100, 1), vslider("freq2k [tooltip:Hz]",2000., 20, 20000, 1),    vslider("fs2k   [tooltip:gain (dB) at 2 kHz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs4k", 50, 1, 100, 1), vslider("freq4k [tooltip:Hz]",4000., 20, 20000, 1),    vslider("fs4k   [tooltip:gain (dB) at 4 kHz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs8k", 50, 1, 100, 1), vslider("freq8k [tooltip:Hz]",8000., 20, 20000, 1),    vslider("fs8k   [tooltip:gain (dB) at 8 kHz]", 0, -40, 30, 0.1): smoothi(0.999))
-          : ifilter(vslider("Qs16k", 50, 1, 100, 1),vslider("freq16k [tooltip:Hz]",16000., 20, 20000, 1),    vslider("fs16k  [tooltip:gain (dB) at 16 kHz]", 0, -40, 30, 0.1): smoothi(0.999))
+process =   ifilter(vslider("Qs31_25", 50, 1, 100, 1),
+                    vslider("freq31_25 [tooltip:Hz]",31., 20, 20000, 1),
+                    vslider("fs31_25[tooltip:gain (dB) at 31.25 Hz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs62_5", 50, 1, 100, 1),
+                    vslider("freq62_5 [tooltip:Hz]",62., 20, 20000, 1),
+                    vslider("fs62_5 [tooltip:gain (dB) at 62.5 Hz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs125", 50, 1, 100, 1),
+                    vslider("freq125 [tooltip:Hz]",125., 20, 20000, 1),
+                    vslider("fs125  [tooltip:gain (dB) at 125 Hz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs250", 50, 1, 100, 1),
+                    vslider("freq250 [tooltip:Hz]",250., 20, 20000, 1),
+                    vslider("fs250  [tooltip:gain (dB) at 250 Hz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs500", 50, 1, 100, 1),
+                    vslider("freq500 [tooltip:Hz]",500., 20, 20000, 1),
+                    vslider("fs500  [tooltip:gain (dB) at 500 Hz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs1k", 50, 1, 100, 1),
+                    vslider("freq1k [tooltip:Hz]",1000., 20, 20000, 1),
+                    vslider("fs1k   [tooltip:gain (dB) at 1 kHz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs2k", 50, 1, 100, 1),
+                    vslider("freq2k [tooltip:Hz]",2000., 20, 20000, 1),
+                    vslider("fs2k   [tooltip:gain (dB) at 2 kHz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs4k", 50, 1, 100, 1),
+                    vslider("freq4k [tooltip:Hz]",4000., 20, 20000, 1),
+                    vslider("fs4k   [tooltip:gain (dB) at 4 kHz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs8k", 50, 1, 100, 1),
+                    vslider("freq8k [tooltip:Hz]",8000., 20, 20000, 1),
+                    vslider("fs8k   [tooltip:gain (dB) at 8 kHz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
+
+          : ifilter(vslider("Qs16k", 50, 1, 100, 1),
+                    vslider("freq16k [tooltip:Hz]",16000., 20, 20000, 1),
+                    vslider("fs16k  [tooltip:gain (dB) at 16 kHz]", 0, -40, 30, 0.1)
+                      : db2linear : smoothi(0.999))
           ;

commit b2991905632f37ed2f4c4106b9bdc565ba158e73

fixed bug in selecteq
introduced in 7dfdb325 (2013-07-03)

trunk/src/faust-generated/selecteq.cc
@@ -203,43 +203,43 @@ void always_inline Dsp::compute(int count, float *input0, float *output0)
 	double 	fSlow59 = (1 + (fSlow2 * (fSlow2 + fSlow1)));
 	for (int i=0; i<count; i++) {
 		fRec0[0] = (fSlow0 + (0.999 * fRec0[1]));
-		double fTemp0 = (fSlow1 * (0 - fRec0[0]));
+		double fTemp0 = (fSlow1 / fRec0[0]);
 		double fTemp1 = (1 + (fSlow2 * (fSlow2 + fTemp0)));
 		double fTemp2 = (fSlow3 * fRec1[1]);
 		fRec2[0] = (fSlow4 + (0.999 * fRec2[1]));
-		double fTemp3 = (fSlow5 * (0 - fRec2[0]));
+		double fTemp3 = (fSlow5 / fRec2[0]);
 		double fTemp4 = (1 + (fSlow6 * (fSlow6 + fTemp3)));
 		double fTemp5 = (fSlow7 * fRec3[1]);
 		fRec4[0] = (fSlow8 + (0.999 * fRec4[1]));
-		double fTemp6 = (fSlow9 * (0 - fRec4[0]));
+		double fTemp6 = (fSlow9 / fRec4[0]);
 		double fTemp7 = (1 + (fSlow10 * (fSlow10 + fTemp6)));
 		double fTemp8 = (fSlow11 * fRec5[1]);
 		fRec6[0] = (fSlow12 + (0.999 * fRec6[1]));
-		double fTemp9 = (fSlow13 * (0 - fRec6[0]));
+		double fTemp9 = (fSlow13 / fRec6[0]);
 		double fTemp10 = (1 + (fSlow14 * (fSlow14 + fTemp9)));
 		double fTemp11 = (fSlow15 * fRec7[1]);
 		fRec8[0] = (fSlow16 + (0.999 * fRec8[1]));
-		double fTemp12 = (fSlow17 * (0 - fRec8[0]));
+		double fTemp12 = (fSlow17 / fRec8[0]);
 		double fTemp13 = (1 + (fSlow18 * (fSlow18 + fTemp12)));
 		double fTemp14 = (fSlow19 * fRec9[1]);
 		fRec10[0] = (fSlow20 + (0.999 * fRec10[1]));
-		double fTemp15 = (fSlow21 * (0 - fRec10[0]));
+		double fTemp15 = (fSlow21 / fRec10[0]);
 		double fTemp16 = (1 + (fSlow22 * (fSlow22 + fTemp15)));
 		double fTemp17 = (fSlow23 * fRec11[1]);
 		fRec12[0] = (fSlow24 + (0.999 * fRec12[1]));
-		double fTemp18 = (fSlow25 * (0 - fRec12[0]));
+		double fTemp18 = (fSlow25 / fRec12[0]);
 		double fTemp19 = (1 + (fSlow26 * (fSlow26 + fTemp18)));
 		double fTemp20 = (fSlow27 * fRec13[1]);
 		fRec14[0] = (fSlow28 + (0.999 * fRec14[1]));
-		double fTemp21 = (fSlow29 * (0 - fRec14[0]));
+		double fTemp21 = (fSlow29 / fRec14[0]);
 		double fTemp22 = (1 + (fSlow30 * (fSlow30 + fTemp21)));
 		double fTemp23 = (fSlow31 * fRec15[1]);
 		fRec16[0] = (fSlow32 + (0.999 * fRec16[1]));
-		double fTemp24 = (fSlow33 * (0 - fRec16[0]));
+		double fTemp24 = (fSlow33 / fRec16[0]);
 		double fTemp25 = (1 + (fSlow34 * (fSlow34 + fTemp24)));
 		double fTemp26 = (fSlow35 * fRec17[1]);
 		fRec18[0] = (fSlow36 + (0.999 * fRec18[1]));
-		double fTemp27 = (fSlow37 * (0 - fRec18[0]));
+		double fTemp27 = (fSlow37 / fRec18[0]);
 		double fTemp28 = (1 + (fSlow38 * (fSlow38 + fTemp27)));
 		double fTemp29 = (fSlow39 * fRec19[1]);
 		fRec19[0] = ((double)input0[i] - (((fRec19[2] * (1 + (fSlow38 * (fSlow38 - fTemp27)))) + fTemp29) / fTemp28));

trunk/src/faust/guitarix.lib
@@ -282,7 +282,7 @@ eqfilter(Q,F,G)	= TF2(  (1 +  K/Q + K*K) 	/ D,
 		};
 
 
-ifilter(Q,F,G) = eqfilter(Q,F,-G);
+ifilter(Q,F,G) = eqfilter(Q,F,1/G);
 
 
 /****************************************************************