360682b70f070348695cb0355ed65df777cae8d4
Andreas Degert <andreas.degert@googlemail.com>
2013-07-11 17:51:03 +0200

    tonestack plugins and selecteq need double precision
    - numerical cancellation in single precision float calculations
    - shows up as noise after an impulse
    - other iir variants are either not stable in single precision
      (e.g. iir_nl for tonestack) or need more cpu time than the standard
      iir in double precision (tf2_np for selecteq).
    - vectorized single precision selecteq needs more cpu time than the
      scalar version
    - add new config option --faust-vectorize-float that only vectorizes
      single precision faust sources (ARM NEON is only single precision)

--- trunk/wscript_helper.py
+++ trunk/wscript_helper.py
@@ -19,6 +19,7 @@ def add_zita_resampler(bld, uselib, sources, incl, base=".."):
 
 def get_faust_args(bld):
     float_arg = ["-s","40000","--float"]
+    double_arg = ["--double"]
     if bld.env['FAUST_DOUBLE']:
         arg = ["--double"]
     else:
@@ -26,9 +27,15 @@ def get_faust_args(bld):
     if bld.env['FAUST_VECTORIZE']:
         float_arg.append('--vectorize')
         arg.append('--vectorize')
+        double_arg.append('--vectorize')
+    elif bld.env['FAUST_VECTORIZE_FLOAT']:
+        float_arg.append('--vectorize')
+        if not bld.env['FAUST_DOUBLE']:
+            arg.append('--vectorize')
     add_args = bld.env['FAUST_OPTIONS']
     if add_args:
         add_args = "--add=%s" % add_args
         float_arg.append(add_args)
+        double_arg.append(add_args)
         arg.append(add_args)
-    return arg, float_arg
+    return arg, float_arg, double_arg
