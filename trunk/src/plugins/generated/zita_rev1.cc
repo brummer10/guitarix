75e202c6b8879b7c1e213188d9ac7438567561b2
Andreas Degert <andreas.degert@googlemail.com>
2013-07-16 22:07:35 +0200

    change the stackbased ui definition for zita_rev1
    For the Guitarix program the glade definition is used; this change
    creates a better webui display.

--- trunk/src/plugins/generated/zita_rev1.cc
+++ trunk/src/plugins/generated/zita_rev1.cc
@@ -1291,7 +1291,10 @@ b.openHorizontalhideBox("");
 b.closeBox();
 b.openVerticalBox("");
 {
-    b.openHorizontalBox("");
+    b.create_small_rackknob(PARAM("output.dry_wet_mix"), 0);
+    b.create_small_rackknob(PARAM("output.level"), 0);
+
+    b.openHorizontalBox("Reverb Time T60");
     {
 	b.create_small_rackknob(PARAM("input.in_delay"), 0);
 	b.create_small_rackknob(PARAM("decay_times.lf_x"), 0);
@@ -1300,14 +1303,18 @@ b.openVerticalBox("");
 	b.create_small_rackknob(PARAM("decay_times.hf_damping"), 0);
     }
     b.closeBox();
-    b.openHorizontalBox("");
+
+    b.openHorizontalBox("Eq1");
     {
 	b.create_small_rackknob(PARAM("equalizer1.eq1_freq"), 0);
 	b.create_small_rackknob(PARAM("equalizer1.eq1_level"), 0);
+    }
+    b.closeBox();
+
+    b.openHorizontalBox("Eq1");
+    {
 	b.create_small_rackknob(PARAM("equalizer2.eq2_freq"), 0);
 	b.create_small_rackknob(PARAM("equalizer2.eq2_level"), 0);
-	b.create_small_rackknob(PARAM("output.dry_wet_mix"), 0);
-	b.create_small_rackknob(PARAM("output.level"), 0);
     }
     b.closeBox();
 }
--- trunk/src/plugins/zita_rev1_ui.cc
+++ trunk/src/plugins/zita_rev1_ui.cc
@@ -5,23 +5,30 @@ b.openHorizontalhideBox("");
 b.closeBox();
 b.openVerticalBox("");
 {
-    b.openHorizontalBox("");
+    b.create_small_rackknob(PARAM("input.in_delay"), 0);
+    b.create_small_rackknob(PARAM("output.dry_wet_mix"), 0);
+    b.create_small_rackknob(PARAM("output.level"), 0);
+
+    b.openHorizontalBox("Reverb Time T60");
     {
-	b.create_small_rackknob(PARAM("input.in_delay"), 0);
 	b.create_small_rackknob(PARAM("decay_times.lf_x"), 0);
 	b.create_small_rackknob(PARAM("decay_times.low_rt60"), 0);
 	b.create_small_rackknob(PARAM("decay_times.mid_rt60"), 0);
 	b.create_small_rackknob(PARAM("decay_times.hf_damping"), 0);
     }
     b.closeBox();
-    b.openHorizontalBox("");
+
+    b.openHorizontalBox("Eq1");
     {
 	b.create_small_rackknob(PARAM("equalizer1.eq1_freq"), 0);
 	b.create_small_rackknob(PARAM("equalizer1.eq1_level"), 0);
+    }
+    b.closeBox();
+
+    b.openHorizontalBox("Eq2");
+    {
 	b.create_small_rackknob(PARAM("equalizer2.eq2_freq"), 0);
 	b.create_small_rackknob(PARAM("equalizer2.eq2_level"), 0);
-	b.create_small_rackknob(PARAM("output.dry_wet_mix"), 0);
-	b.create_small_rackknob(PARAM("output.level"), 0);
     }
     b.closeBox();
 }
