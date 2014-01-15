e85c4289635aba84bcfc5c081579321926df601d
Andreas Degert <andreas.degert@googlemail.com>
2014-01-15 17:28:37 +0100

    added effect units: GCB95 and JCM800Pre

--- trunk/src/gx_head/engine/gx_engine.cpp
+++ trunk/src/gx_head/engine/gx_engine.cpp
@@ -321,6 +321,8 @@ void GxEngine::load_static_plugins() {
     pl.add(pluginlib::mbe::plugin(),              PLUGIN_POS_RACK, PGN_GUI);
     pl.add(pluginlib::mbdel::plugin(),            PLUGIN_POS_RACK, PGN_GUI);
     pl.add(pluginlib::mbchor::plugin(),           PLUGIN_POS_RACK, PGN_GUI);
+    pl.add(pluginlib::jcm800pre::plugin(),        PLUGIN_POS_RACK, PGN_GUI);
+    pl.add(pluginlib::gcb_95::plugin(),           PLUGIN_POS_RACK, PGN_GUI);
     // stereo
     pl.add(gx_effects::chorus::plugin(),          PLUGIN_POS_RACK, PGN_GUI);
     pl.add(gx_effects::flanger::plugin(),         PLUGIN_POS_RACK, PGN_GUI);
