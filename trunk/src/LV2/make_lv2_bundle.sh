b3a092aca97a7bf5ada0f8819b2becb86d9e5320
hermann <brummer-@web.de>
2014-01-25 15:10:31 +0100

    * add some more automation to make_lv2_bundle.sh. The only task leave is, add the ports into the ttl file.

--- trunk/src/LV2/make_lv2_bundle.sh
+++ trunk/src/LV2/make_lv2_bundle.sh
@@ -1,15 +1,54 @@
 #! /bin/bash
 
 # use to copy the LV2 bundle gx_sceleton.lv2 to gx_${name}.lv2
-# were ${name} is the first arg on comandline, usualy the dsp file name.
+# were ${name} is the first arg on comandline, usualy the dsp file.
 # the dsp file needs to be stored in the faust folder.
 
-name=$*
-mkdir -p gx_${name}.lv2
-cp -r gx_sceleton.lv2/* gx_${name}.lv2/ 
-cd ./gx_${name}.lv2 && rename 's/sceleton/'${name}'/g' * && sed -i 's/sceleton/'${name}'/g' *
+function usage() {
+  echo "usage: make_lv2_bundle {options} <dsp-file> [<module-name>]"
+  echo "options:"
+  echo "    -s:   faust use single precision"
+  echo "    -d:   faust use double precision (default)"
+  echo "    -V:   faust use vectorize"
+  echo "    -S x: faust use vector size x"
+  exit 1
+}
+tooldir=../../tools
+instdir="$tooldir"/.."$(dirname "$0")"
+prec="--double"
+faustopt=()
+copy=0
+while getopts sdVS:c OPT; do
+  case "$OPT" in
+  h) usage;;
+  s) prec="--float";;
+  d) prec="--double";;
+  V) faustopt+=(--vectorize);;
+  S) faustopt+=(--add="-vs $OPTARG");;
+  c) copy=1;;
+  \?) usage;;
+  esac
+done
+shift $(expr $OPTIND - 1)
+[ "$1" = "" ] && usage
+if [ "$2" = "" ]; then
+  bname="$(basename "$1" .dsp)"
+else
+  bname="$2"
+fi
+rm -rf gx_${bname}.lv2
+mkdir -p gx_${bname}.lv2
 
+set -e
+"$tooldir"/dsp2cc  --init-type=plugin-lv2  \
+  $prec "${faustopt[@]}" -o gx_${bname}.lv2/"$bname.cc" ./faust/"$1"
+
+cp -r gx_sceleton.lv2/* gx_${bname}.lv2/ 
+cd ./gx_${bname}.lv2 && rename 's/sceleton/'${bname}'/g' * && sed -i 's/sceleton/'${bname}'/g' *
+
+cat "$bname.cc" | sed -n '/enum/,/PortIndex/p' |  sed '/enum/d;/PortIndex/d;/{/d;/}/d'>ports
+
+sed -i -e '/EFFECTS_INPUT/r ports' "gx_$bname.h"
+rm -rf ports
  # TO DO
- # add ${name}.dsp to wscript in /faust directory
- # add used ports from generated ${name}.cc file to ${name}.h struct PortIndex
- # and to gx_${name}.ttl
+ # add ports to bname.ttl 
