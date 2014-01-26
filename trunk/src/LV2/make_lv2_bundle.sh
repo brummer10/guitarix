9d2bc2abe2d34ad037f7be82cb126759b6c296f2
hermann <brummer-@web.de>
2014-01-26 19:08:14 +0100

    * add some output to make_lv2_bundle.sh

--- trunk/src/LV2/make_lv2_bundle.sh
+++ trunk/src/LV2/make_lv2_bundle.sh
@@ -3,6 +3,9 @@
 # use to copy the LV2 bundle gx_sceleton.lv2 to gx_${name}.lv2
 # were ${name} is the first arg on comandline, usualy the dsp file.
 # the dsp file needs to be stored in the faust folder.
+BLUE="\033[1;34m"
+RED="\033[1;31m"
+NONE="\033[0m"
 
 function usage() {
   echo "usage: make_lv2_bundle {options} <dsp-file> [<module-name>]"
@@ -41,35 +44,36 @@ fi
 if [ ! -d gx_${bname}.lv2 ]; then
   mkdir -p gx_${bname}.lv2
 else
-  echo "Directory gx_${bname}.lv2 allready exist, should we remove it? Yes/No"
+  echo -e $RED"Directory gx_${bname}.lv2 allready exist, should we remove it? Yes/No"$NONE
   select yn in "Yes" "No"; do
     case $yn in
         Yes ) rm -rf gx_${bname}.lv2; mkdir -p gx_${bname}.lv2; break;;
-        No ) echo "exit"; exit;;
+        No ) echo  -e $RED"exit"$NONE; exit;;
     esac
   done
 fi
 
+echo -e $BLUE"generate $bname.cc and copy to gx_$bname.lv2"$NONE
 set -e
 "$tooldir"/dsp2cc  --init-type=plugin-lv2  \
   $prec "${faustopt[@]}" -o gx_${bname}.lv2/"$bname.cc" ${faustdir}/"$1"
 
+echo "copy gx_sceleton.lv2 to gx_$bname.lv2 and rename/replace strings to $bname"
 cp -r gx_sceleton.lv2/* gx_${bname}.lv2/ 
 cd ./gx_${bname}.lv2 && rename 's/sceleton/'${bname}'/g' * && sed -i 's/sceleton/'${bname}'/g' *
 
+echo "grep ports and copy them to  gx_$bname.h"
 cat "$bname.cc" | sed -n '/enum/,/PortIndex/p' |  sed '/enum/d;/PortIndex/d;/{/d;/}/d'>ports
 sed -i -e '/EFFECTS_INPUT/r ports' "gx_$bname.h"
 
+echo "grep ports values and enums and copy them to gx_$bname.ttl"
 cat "$bname.cc" | sed -n '/data;/{p;g;1!p;};h' | sed 's/ , /\n/;s/.*\n//;s/case//g;s/,/ ;/g;s/://g;s/	 //g;s/$/;/' | sed '$!N;s/\n/ /'>ports
 cat "$bname.cc" | sed -n '/value_pair/{p;n;1!p;};h' | sed 's/{/\n/;s/.*\n//;s/ , /\n/;s/.*\n//;s/case//g;s/}//g;s/{//g;s/;//g;s/,/ ;/g;s/://g;s/	 //g;s/;0//g;s/$/;/' > enums
 
-enum_var=$(echo '        lv2:portProperty lv2:integer;
-        lv2:portProperty lv2:enumeration ; ')
 j=2;
-d=0
 match=0
 
-while IFS=$';' read -r -a myArray
+while IFS=$';' read -r -a myPorts
 do
   while IFS=$';' read -r -a myEnum
   do
@@ -79,22 +83,20 @@ do
       a=${#myEnum[@]} 
       if (($a==1))
       then
-        if [ ${myArray[4]} == ${myEnum[0]} ]
+        if [ ${myPorts[4]} == ${myEnum[0]} ]
         then
           match=1
           break
         fi
       else
         match=0
-        FLOAT=${myArray[1]}
+        FLOAT=${myPorts[1]}
         i=${FLOAT/\.*}
         for (( c=0; c<a; c++ ))
         do 
-          enum_var1+="        lv2:scalePoint [rdfs:label "${myEnum[d]}"; rdf:value "${i}"];\n"
+          enum_var1+="        lv2:scalePoint [rdfs:label "${myEnum[c]}"; rdf:value "${i}"];\n"
           i=$[i+1]
-          d=$[d+1]
         done
-        d=0
       fi
     fi
   sed -i -e "1d" enums
@@ -103,11 +105,11 @@ do
         a lv2:InputPort ,
             lv2:ControlPort ;
         lv2:index' $j' ;
-        lv2:symbol "'${myArray[4]}'" ;
-        lv2:name "'${myArray[4]}'" ;'
-  echo '        lv2:default '${myArray[0]}' ;
-        lv2:minimum '${myArray[1]}' ;
-        lv2:maximum '${myArray[2]}' ;'
+        lv2:symbol "'${myPorts[4]}'" ;
+        lv2:name "'${myPorts[4]}'" ;'
+  echo '        lv2:default '${myPorts[0]}' ;
+        lv2:minimum '${myPorts[1]}' ;
+        lv2:maximum '${myPorts[2]}' ;'
   if (($match==1))
   then
     echo '        lv2:portProperty lv2:integer;
@@ -124,4 +126,6 @@ rm -rf ports
 rm -rf enums
 
 
-echo "Okay, gx_${bname}.lv2 is done" 
+echo -e $BLUE"Okay, gx_${bname}.lv2 is done"$NONE
+echo  -e $BLUE"Please add gx_${bname}.lv2 to the wscript in the LV2 directory
+and run ./waf build in order to build the plug"$NONE
