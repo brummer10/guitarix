d3da5e0b7814194d8fda9f47ebc04ed8bffd08b4
drgreenthumb <thisisadrgreenthumb@gmail.com>
2014-06-24 20:29:28 +0400

    Add switched tremolo plugin

---
+++ trunk/src/LV2/faust/switched_tremolo.dsp
@@ -0,0 +1,38 @@
+//------------------------------------
+//Inspired by:Line 6 POD-3
+//Found this effect somewhere here
+//------------------------------------
+
+//------------------------------------
+//Description:
+//The modulated signal got using 4 oscillations,
+//which are switched.
+//
+//Parameters description:
+//steps - number of oscillators in bank
+//sw_freq - oscillators switching freq
+//freq0..4 - oscillators freqs
+//depth - as is
+//------------------------------------
+
+import("math.lib");
+import("oscillator.lib");
+
+steps = hslider("Steps", 4, 1, 4, 1);
+sw_freq = hslider("Switch Freq", 1, 0.25, 5, 0.25);
+freq0 = hslider("Freq 0", 1, 0.25, 15, 0.5);
+freq1 = hslider("Freq 1", 1, 0.25, 15, 0.5);
+freq2 = hslider("Freq 2", 1, 0.25, 15, 0.5);
+freq3 = hslider("Freq 3", 1, 0.25, 15, 0.5);
+depth = hslider("Depth", 0.5, 0, 1, 0.05);
+
+in_range(min_val, max_val, x) = x>min_val,x<=max_val:*:_;
+
+N = 4;
+freqs = (freq0, freq1, freq2, freq3);
+process = (par(i,N,osc(take(i+1,freqs))),
+	(lf_sawpos(sw_freq)<:par(i,N,in_range(i/steps,(i+1)/steps):
+	smooth(tau2pole(0.05))))),_:
+	(dot(N):_*depth:_+1:_-depth),_:_*_:_;
+
+
---
+++ trunk/src/LV2/gx_switched_tremolo.lv2/gx_switched_tremolo.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2012 Hermann Meyer, Andreas Degert, Pete Shorthose, Steve Poskitt
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * --------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#ifndef SRC_HEADERS_GXEFFECTS_H_
+#define SRC_HEADERS_GXEFFECTS_H_
+
+#include <lv2.h>
+
+#define GXPLUGIN_URI "http://guitarix.sourceforge.net/plugins/gx_switched_tremolo_"
+#define GXPLUGIN_UI_URI "http://guitarix.sourceforge.net/plugins/gx_switched_tremolo_#gui"
+
+
+typedef enum
+{
+   EFFECTS_OUTPUT,
+   EFFECTS_INPUT,
+   DEPTH, 
+   FREQ0, 
+   FREQ1, 
+   FREQ2, 
+   FREQ3, 
+   STEPS, 
+   SWITCHFREQ, 
+} PortIndex;
+
+#endif //SRC_HEADERS_GXEFFECTS_H_
