d749505206f0c26fd76d2607e99137dc34080e30
Hermann Meyer <brummer-@web.de>
2015-02-02 11:14:45 +0100

    * delete old factory preset banks  * add new factory preset bank "brummer"

--- trunk/factorysettings/dirlist.js
+++ trunk/factorysettings/dirlist.js
@@ -1,8 +1,3 @@
 [
-    ["kokoko3k",    "kokoko3k.gx"],
-    ["funkmuscle",  "funkmuscle.gx"],
-    ["zettberlin",  "zettberlin.gx"],
-    ["autoandimat", "autoandimat.gx"],
-    ["StudioDave",  "dlp_ae.gx"],
-    ["JP",          "jp_n_o_s.gx"]
+    ["brummer",     "brummer.gx"]
 ]
