7fbb9573397f53656d12bf4f0f0b4cd8a2b8cac6
Hermann Meyer <brummer-@web.de>
2015-04-26 15:21:10 +0200

    * add make lv2 bundle with GUI script and skeleton files

--- trunk/src/LV2/make_lv2_bundle.sh
+++ trunk/src/LV2/make_lv2_bundle.sh
@@ -17,6 +17,7 @@ function usage() {
   echo "    -V:   faust use vectorize"
   echo "    -S x: faust use vector size x"
   echo "    -p /: path/to/source/  sourcefile "
+  echo "    -n    effect name "
   exit 1
 }
 
@@ -27,21 +28,21 @@ case "$extension" in
       bname="$(basename "$1" .dsp)"
     else
       bname="$(basename "$1" .dsp)"
-      effect_name="$2"
+      effect_category="$2"
     fi;;
   cc)
     if [ "$2" = "" ]; then
       bname="$(basename "$1" .cc)"
     else
       bname="$(basename "$1" .cc)"
-      effect_name="$2"
+      effect_category="$2"
     fi;;
   cpp)
     if [ "$2" = "" ]; then
       bname="$(basename "$1" .cpp)"
     else
       bname="$(basename "$1" .cpp)"
-      effect_name="$2"
+      effect_category="$2"
     fi;;
   *)
    echo -e $RED"file extension not supported"$NONE ; usage;;
@@ -54,7 +55,7 @@ function select_plugin_type() {
     if [[ $type == "quit" ]]; then
       break
     fi
-    effect_name="$type"
+    effect_category="$type"
     if [ ! -z "$type" ]; then
       break;
     else
@@ -65,7 +66,7 @@ function select_plugin_type() {
 
 function check_plugin_type() { 
   for I in "${Plugin_types[@]}"; do
-    [[ $I == $effect_name ]] && return 0
+    [[ $I == $effect_category ]] && return 0
   done
   select_plugin_type
   return 0
@@ -109,11 +110,11 @@ function copy_sceleton() {
   fi
 
   if [ "$stereo" == "false" ] ; then
-    echo "copy gx_sceleton.lv2 to gx_$bname.lv2 and rename/replace strings to $bname"
+    echo -e "copy gx_sceleton.lv2 to "$BLUE"gx_$bname.lv2"$NONE" and rename/replace strings to "$BLUE"$bname"$NONE
     cp -r gx_sceleton.lv2/* gx_${bname}.lv2/ 
     j=2
   else
-    echo "copy gx_sceleton_stereo.lv2 to gx_$bname.lv2 and rename/replace strings to $bname"
+    echo -e "copy gx_sceleton_stereo.lv2 to "$BLUE"gx_$bname.lv2"$NONE" and rename/replace strings to "$BLUE"$bname"$NONE
     cp -r gx_sceleton_stereo.lv2/* gx_${bname}.lv2/ 
     j=4
   fi
@@ -121,7 +122,7 @@ function copy_sceleton() {
 }
 
 function grep_ports_enums() {
-  echo "grep ports and copy them to  gx_$bname.h"
+  echo -e "grep ports and copy them to  "$BLUE"gx_$bname.h"$NONE
   cat "$bname.cc" | sed -n '/enum/,/PortIndex/p' |  sed '/enum/d;/PortIndex/d;/{/d;/}/d'>ports
 
   if [ "$stereo" == "false" ] ; then
@@ -129,9 +130,71 @@ function grep_ports_enums() {
   else
     sed -i -e '/EFFECTS_INPUT1/r ports' "gx_$bname.h"
   fi
-  echo "grep ports values and enums and copy them to gx_$bname.ttl"
+  echo -e "grep ports values and enums and copy them to "$BLUE"gx_$bname.ttl"$NONE
   cat "$bname.cc" | sed -n '/data;/{p;g;1!p;};h' | sed 's/ , /\n/;s/.*\n//;s/case//g;s/,/ ;/g;s/://g;s/	 //g;s/  //g;s/ //g;s/$/;/' | sed '$!N;s/\n//'>ports
   cat "$bname.cc" | sed -n '/value_pair/{p;n;1!p;};h' | sed 's/{/\n/;s/.*\n//;s/ , /\n/;s/.*\n//;s/case//g;s/}//g;s/{//g;s/;//g;s/,/ ;/g;s/://g;s/	 //g;s/;0//g;s/  //g;s/ //g;s/$/;/' > enums
+}
+
+function make_ui() {
+  echo "generate GUI"
+  COUNTER=0
+  ECOUNTER=0
+  match=0
+  enum_var1=""
+  enum_var=""
+  cat enums >enums1
+  while IFS=$';' read -r -a line; do
+    while IFS=$';' read -r -a myEnum; do
+      if [ -z "${myEnum[0]}" ]; then
+        match=0
+      else
+        a=${#myEnum[@]} 
+        if (($a==1)); then
+          if [ ${line[4]} == ${myEnum[0]} ]; then
+            match=1
+            sed -i -e "1,2d" enums1
+            break
+          fi
+        else
+          match=0
+          FLOAT=${myPorts[1]}
+          i=${FLOAT/\.*}
+          i=${i/e/}
+          enum_var1+="Glib::ustring enum"${line[4]}"[] = {"
+          for (( c=0; c<a; c++ )); do 
+            enum_var1+="${myEnum[c]},"
+            i=$[i+1]
+          done
+          enum_var2="${enum_var1::-1}" 
+          enum_var2+="};\n"
+        fi
+      fi
+    done < enums1
+    if (($match==1)); then
+      enum_var+='  '$enum_var2
+      enum_var+="  static const size_t enum"${line[4]}"_size = sizeof(enum"${line[4]}") / sizeof(enum"${line[4]}"[0]);\n"
+      enum_var+="  make_selector_box(\&m_vboxs["${ECOUNTER}"],\""${line[4]}"\", enum"${line[4]}", enum"${line[4]}"_size, 0,1.0, "${line[4]}");\n"
+      ports+='    case '${line[4]}':\n      return \&m_selector['${ECOUNTER}'];\n'
+      let ECOUNTER=ECOUNTER+1 
+      enum_var1=""
+      match=0
+    else
+      enum_var1=""
+      conts+='  make_controller_box(\&m_vboxc['${COUNTER}'], \"'${line[4]}'\", '${line[1]}', '${line[2]}', '${line[3]}', '${line[4]}');\n'
+      ports+='    case '${line[4]}':\n      return \&m_bigknob['${COUNTER}'];\n'
+      let COUNTER=COUNTER+1 
+    fi
+  done < ports
+  sed -i "s/PORTS/${ports}/g;s/CONTS/${conts}/g;s/VARI/${COUNTER}/g;s#ENUMS#${enum_var}#g;s/VAI/${ECOUNTER}/g;" widget.cpp  
+  sed -i "s/VAR/${COUNTER}/g;s/VAI/${ECOUNTER}/g;" widget.h
+  rm -rf enums1
+  if [ ! -z "$effect_name" ]; then
+    sed -i 's/EffectNAME/'"${effect_name}"'/g'  gx_${bname}_gui.cpp
+    echo -e "set plugin name to "$BLUE"Gx$effect_name"$NONE
+  else
+    sed -i 's/EffectNAME/'${bname}'/g'  gx_${bname}_gui.cpp
+    echo -e "set plugin name to "$BLUE"Gx$bname"$NONE
+  fi
 }
 
 function make_ttl() {
@@ -181,18 +244,21 @@ function make_ttl() {
     j=$[j+1]
     enum_var1=""
   done < ports >> gx_$bname.ttl
-  echo ";
-    mod:gui [
-        a mod:Gui;
-        mod:resourcesDirectory <modgui>;
-        mod:iconTemplate <modgui/gx$bname.html>;
-        mod:templateData <modgui/gx$bname.json>;
-        mod:screenshot <modgui/gx$bname.png>;
-        mod:thumbnail <modgui/gx$bname-thumb.png>;
-    ]." >> gx_$bname.ttl
+  echo " .
+
+<http://guitarix.sourceforge.net/plugins/gx_${bname}_gui#_${bname}_>
+  a guiext:GtkUI;
+  guiext:binary <gx_${bname}_gui.so>;
+  guiext:requiredFeature guiext:makeResident;
+  ." >> gx_$bname.ttl
+  if [ ! -z "$effect_category" ]; then
+    sed -i 's/EffectPlugin/'${effect_category}'/g'  gx_${bname}.ttl
+    echo -e "set plugin class to "$BLUE"$effect_category"$NONE
+  fi
   if [ ! -z "$effect_name" ]; then
-    sed -i 's/EffectPlugin/'${effect_name}'/g'  gx_$bname.ttl
-    echo -e "set plugin class to "$BLUE"$effect_name"$NONE
+    sed -i 's/EffectNAME/'"${effect_name}"'/g'  gx_${bname}.ttl
+  else
+    sed -i 's/EffectNAME/'${bname}'/g'  gx_${bname}.ttl
   fi
   rm -rf ports
   rm -rf enums
@@ -262,35 +328,46 @@ prec="--double"
 faustopt=()
 copy=0
 bname=""
+effect_category=""
 effect_name=""
 
 ############################# main #####################################
 
-while getopts hsdVSp:c OPT; do
+while getopts hn:sdVSp:c OPT; do
+  sc=0
   case "$OPT" in
   h) usage;;
+  n) effect_name=$OPTARG;;
   s) prec="--float";;
   d) prec="--double";;
   V) faustopt+=(--vectorize);;
   S) faustopt+=(--add="-vs $OPTARG");;
-  p) faustdir=$OPTARG;;
+  p) faustdir=$2
+     file=$3
+     sc=1;;
   c) copy=1;;
   \?) usage;;
   esac
+  if [ $OPTIND != 1 ] # This test fails only if multiple options are stacked
+                        # after a single -.
+  then    shift $((OPTIND - 1 + sc))
+     OPTIND=1
+  fi
 done
 
-shift $(expr $OPTIND - 1)
-[ "$1" = "" ] && usage
-extension="${1##*.}"
+#shift $(expr $OPTIND - 1)
+#[ "$1" = "" ] && usage
+extension="${file##*.}"
 
 ######################## function calls ################################
 
-get_basename_ext "$1" "$2"
-check_plugin_type && echo -e "Plugin type "$BLUE"$effect_name"$NONE
+get_basename_ext "$file" "$2"
+check_plugin_type && echo -e "Plugin type "$BLUE"$effect_category"$NONE
 check_dir
 dsptocc
 copy_sceleton
 grep_ports_enums
+make_ui
 make_ttl
 byby
 
