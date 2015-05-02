f4afeec3ff9ff994374e65d3b788e9b7a0dd65b9
Hermann Meyer <brummer-@web.de>
2015-05-02 15:26:31 +0200

    * add gain control to Roland Double Beat Fuzz section

--- trunk/src/plugins/rolandwah_ui.cc
+++ trunk/src/plugins/rolandwah_ui.cc
@@ -7,6 +7,7 @@ b.openHorizontalBox("");
     b.create_selector(PARAM("mode"), "Mode");
     b.create_small_rackknobr(PARAM("lfobpm"), "Alien Freq");
     b.create_small_rackknobr(PARAM("Fuzz"), "Fuzz");
+    b.create_small_rackknobr(PARAM("Gain"), "Gain");
     b.create_small_rackknobr(PARAM("Wah"), "Wah");
     b.create_small_rackknobr(PARAM("wet_dry"), "dry/wet");
 b.closeBox();
