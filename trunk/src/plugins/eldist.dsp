f4f4c6125dca5f499f0dde84ec31e5e6e44ccb37
Hermann Meyer <brummer-@web.de>
2015-08-13 19:25:36 +0200

    * add missing files

---
+++ trunk/src/plugins/eldist.dsp
@@ -0,0 +1,29 @@
+// generated automatically
+// DO NOT MODIFY!
+declare id "eldist";
+declare name "Electra Distortion";
+declare category "Distortion";
+declare shortname "El.Distortion";
+declare description "Electra Distortion";
+
+import("filter.lib");
+
+process = pre : iir((b0/a0,b1/a0),(a1/a0)) : clip with {
+    LogPot(a, x) = if(a, (exp(a * x) - 1) / (exp(a) - 1), x);
+    Inverted(b, x) = if(b, 1 - x, x);
+    s = 0.993;
+    fs = float(SR);
+    pre = _;
+    clip(x) = (min(0.4514,max(-0.4514,x)));
+
+    
+        Drive = vslider("Drive[name:Drive]", 0.5, 0, 1, 0.01) : Inverted(0) : LogPot(0) : smooth(s);
+    
+    b0 = -8.14686408743197e-8*Drive*fs - 0.00018716364572377*fs;
+
+    b1 = 8.14686408743197e-8*Drive*fs + 0.00018716364572377*fs;
+
+    a0 = Drive*(-3.9949101411109e-5*fs - 8.66687668918243e-5) + 4.07955525542246e-5*fs + 0.000635245647283505;
+
+    a1 = Drive*(3.9949101411109e-5*fs - 8.66687668918243e-5) - 4.07955525542246e-5*fs + 0.000635245647283505;
+};
---
+++ trunk/src/plugins/generated/eldist.cc
@@ -0,0 +1,158 @@
+// generated from file '../src/plugins/eldist.dsp' by dsp2cc:
+// Code generated with Faust 0.9.65 (http://faust.grame.fr)
+
+#include "gx_faust_support.h"
+#include "gx_plugin.h"
+
+namespace pluginlib {
+namespace eldist {
+
+class Dsp: public PluginDef {
+private:
+	int fSamplingFreq;
+	FAUSTFLOAT 	fslider0;
+	double 	fRec0[2];
+	double 	fConst0;
+	double 	fConst1;
+	double 	fConst2;
+	double 	fConst3;
+	double 	fConst4;
+	double 	fConst5;
+	double 	fConst6;
+	double 	fRec1[2];
+	void clear_state_f();
+	int load_ui_f(const UiBuilder& b, int form);
+	void init(unsigned int samplingFreq);
+	void compute(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0);
+	int register_par(const ParamReg& reg);
+
+	static void clear_state_f_static(PluginDef*);
+	static int load_ui_f_static(const UiBuilder& b, int form);
+	static void init_static(unsigned int samplingFreq, PluginDef*);
+	static void compute_static(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0, PluginDef*);
+	static int register_params_static(const ParamReg& reg);
+	static void del_instance(PluginDef *p);
+public:
+	Dsp();
+	~Dsp();
+};
+
+
+
+Dsp::Dsp()
+	: PluginDef() {
+	version = PLUGINDEF_VERSION;
+	flags = 0;
+	id = "eldist";
+	name = N_("Electra Distortion");
+	groups = 0;
+	description = N_("Electra Distortion"); // description (tooltip)
+	category = N_("Distortion");       // category
+	shortname = N_("El.Distortion");     // shortname
+	mono_audio = compute_static;
+	stereo_audio = 0;
+	set_samplerate = init_static;
+	activate_plugin = 0;
+	register_params = register_params_static;
+	load_ui = load_ui_f_static;
+	clear_state = clear_state_f_static;
+	delete_instance = del_instance;
+}
+
+Dsp::~Dsp() {
+}
+
+inline void Dsp::clear_state_f()
+{
+	for (int i=0; i<2; i++) fRec0[i] = 0;
+	for (int i=0; i<2; i++) fRec1[i] = 0;
+}
+
+void Dsp::clear_state_f_static(PluginDef *p)
+{
+	static_cast<Dsp*>(p)->clear_state_f();
+}
+
+inline void Dsp::init(unsigned int samplingFreq)
+{
+	fSamplingFreq = samplingFreq;
+	fConst0 = double(min(192000, max(1, fSamplingFreq)));
+	fConst1 = (3.9949101411109e-05 * fConst0);
+	fConst2 = (0 - (8.66687668918243e-05 + fConst1));
+	fConst3 = (4.07955525542246e-05 * fConst0);
+	fConst4 = (0.000635245647283505 + fConst3);
+	fConst5 = (fConst1 - 8.66687668918243e-05);
+	fConst6 = (0.000635245647283505 - fConst3);
+	clear_state_f();
+}
+
+void Dsp::init_static(unsigned int samplingFreq, PluginDef *p)
+{
+	static_cast<Dsp*>(p)->init(samplingFreq);
+}
+
+void always_inline Dsp::compute(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0)
+{
+	double 	fSlow0 = (0.007000000000000006 * double(fslider0));
+	for (int i=0; i<count; i++) {
+		fRec0[0] = (fSlow0 + (0.993 * fRec0[1]));
+		double fTemp0 = (fConst4 + (fConst2 * fRec0[0]));
+		double fTemp1 = (8.14686408743197e-08 * fRec0[0]);
+		fRec1[0] = ((double)input0[i] - ((fRec1[1] * (fConst6 + (fConst5 * fRec0[0]))) / fTemp0));
+		output0[i] = (FAUSTFLOAT)min(0.4514, max(-0.4514, (fConst0 * (((fRec1[0] * ((0 - fTemp1) - 0.00018716364572377)) + (fRec1[1] * (0.00018716364572377 + fTemp1))) / fTemp0))));
+		// post processing
+		fRec1[1] = fRec1[0];
+		fRec0[1] = fRec0[0];
+	}
+}
+
+void __rt_func Dsp::compute_static(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0, PluginDef *p)
+{
+	static_cast<Dsp*>(p)->compute(count, input0, output0);
+}
+
+int Dsp::register_par(const ParamReg& reg)
+{
+	reg.registerVar("eldist.Drive",N_("Drive"),"S","",&fslider0, 0.5, 0.0, 1.0, 0.01);
+	return 0;
+}
+
+int Dsp::register_params_static(const ParamReg& reg)
+{
+	return static_cast<Dsp*>(reg.plugin)->register_par(reg);
+}
+
+inline int Dsp::load_ui_f(const UiBuilder& b, int form)
+{
+    if (form & UI_FORM_STACK) {
+#define PARAM(p) ("eldist" "." p)
+
+b.openHorizontalhideBox("");
+    b.create_master_slider(PARAM("Drive"), "Drive");
+b.closeBox();
+b.openHorizontalBox("");
+
+    b.create_small_rackknobr(PARAM("Drive"), "Drive");
+b.closeBox();
+
+#undef PARAM
+        return 0;
+    }
+	return -1;
+}
+
+int Dsp::load_ui_f_static(const UiBuilder& b, int form)
+{
+	return static_cast<Dsp*>(b.plugin)->load_ui_f(b, form);
+}
+PluginDef *plugin() {
+	return new Dsp();
+}
+
+void Dsp::del_instance(PluginDef *p)
+{
+	delete static_cast<Dsp*>(p);
+}
+
+} // end namespace eldist
+} // end namespace pluginlib
