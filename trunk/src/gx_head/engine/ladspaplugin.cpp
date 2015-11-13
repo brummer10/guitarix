86dbd3f87f0f012579cb318ebecc68f831bfa3e7
Hermann Meyer <brummer-@web.de>
2015-11-13 08:02:09 +0100

    * print plugin name in error message when it fail to instantiate

--- trunk/src/gx_head/engine/ladspaplugin.cpp
+++ trunk/src/gx_head/engine/ladspaplugin.cpp
@@ -710,7 +710,7 @@ int Lv2Dsp::activate(bool start, PluginDef *plugin) {
 	return 0;
     }
     if (!self.instance) { 
-		gx_print_warning("Lv2Dsp", ustring::compose("cant activate plugin %1", self.pd->path));
+		gx_print_warning("Lv2Dsp", ustring::compose("cant activate plugin %1", self.name));
 		return 1;
 	}
     self.is_activated = start;
@@ -767,7 +767,7 @@ void Lv2Dsp::init(unsigned int samplingFreq, PluginDef *pldef) {
     }
     self.instance = lilv_plugin_instantiate(self.plugin, samplingFreq, 0);
     if (!self.instance) { 
-		gx_print_error("Lv2Dsp", ustring::compose("cant init plugin %1", self.pd->path));
+		gx_print_error("Lv2Dsp", ustring::compose("cant init plugin: %1 \n uri: %2", self.name, self.pd->path));
 		return;
 	}
     int n = 0;
