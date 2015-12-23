679c94347dc1733c93cb0de8b9de75a4a8e7401e
Hermann Meyer <brummer-@web.de>
2015-12-23 08:15:12 +0100

    * small modification to the Rat and to the FuzzDrive plug

--- trunk/src/plugins/aclipper.dsp
+++ trunk/src/plugins/aclipper.dsp
@@ -2,11 +2,12 @@ declare id 		"aclipper";
 declare name            "Rat";
 declare category        "Distortion";
 
+SR = component("math.lib").SR;
+s = 0.993;
+fs = float(SR);
+drive =  hslider("drive[name:Drive]", 0.5, 0, 1, 0.01) : component("filter.lib").smooth(s);
 
 rat_in = component("filter.lib").iir((b0/a0,b1/a0,b2/a0),(a1/a0,a2/a0)) with {
-    SR = component("math.lib").SR;
-    s = 0.993;
-    fs = float(SR);
 
     b0 = 1.82432723786905e-5*fs;
 
@@ -22,9 +23,6 @@ rat_in = component("filter.lib").iir((b0/a0,b1/a0,b2/a0),(a1/a0,a2/a0)) with {
 };
 
 rat_out = component("filter.lib").iir((b0/a0,b1/a0),(a1/a0)) with {
-    SR = component("math.lib").SR;
-    s = 0.993;
-    fs = float(SR);
 
     b0 = 2.08332871602678e-5*fs;
 
@@ -36,9 +34,6 @@ rat_out = component("filter.lib").iir((b0/a0,b1/a0),(a1/a0)) with {
 };
 
 rat_tone =  component("filter.lib").iir((b0/a0,b1/a0),(a1/a0)) with {
-    SR = component("math.lib").SR;
-    s = 0.993;
-    fs = float(SR);
 
     Tone = 1.0 - vslider("tone[name:Tone]", 0.5, 0, 1, 0.01) : component("filter.lib").smooth(s);
     
@@ -51,12 +46,25 @@ rat_tone =  component("filter.lib").iir((b0/a0,b1/a0),(a1/a0)) with {
     a1 = 3.91923990498812e-5*Tone*fs - 3.91923990498812e-5*fs + 0.0593824228028504;
 };
 
-process = rat_in : X3 : rat_out : *(fulldrive): aclip : rat_tone : *(gain)  with {
-    SR = component("math.lib").SR;
-    s = 0.993;
+ratdrive = component("filter.lib").iir((b0/a0,b1/a0,b2/a0),(a1/a0,a2/a0)) with {
+
+    Volume = 0.5 + drive * 0.75 ;
+    
+    b0 = -9.26800656732889e-10*Volume*pow(fs,2);
+
+    b1 = 1.85360131346578e-9*Volume*pow(fs,2);
+
+    b2 = -9.26800656732889e-10*Volume*pow(fs,2);
+
+    a0 = fs*(4.3384046341364e-10*fs + 8.99037897457717e-9) + 3.41041934946762e-8;
+
+    a1 = -8.6768092682728e-10*pow(fs,2) + 6.82083869893523e-8;
+
+    a2 = fs*(4.3384046341364e-10*fs - 8.99037897457717e-9) + 3.41041934946762e-8;
+};
+
+process = rat_in : X3 : rat_out : ratdrive : clip : rat_tone : *(gain)  with {
     R1 = 4700;
-    drive =  hslider("drive[name:Drive]", 0.5, 0, 1, 0.01);
-    fulldrive = (1.0 + max(0.0, drive-0.25));
     R2 = 1000 + 100000 * drive;
     C = 0.047 * 1e-6;
     a1 = (R1 + R2) * C * 2 * SR;
@@ -69,6 +77,6 @@ process = rat_in : X3 : rat_out : *(fulldrive): aclip : rat_tone : *(gain)  with
     opamp = ffunction(float opamp(float), "clipping.h", "");
     X3 = _ <: _ - opamp(X2-_) :> _ ;
     asymclip = ffunction(float asymhardclip2(float), "clipping.h", "");
-    aclip = asymclip(_);
+    clip = asymclip(_);
     gain = hslider("level[name:Level]", -2, -20, 12, 0.1) : component("music.lib").db2linear : component("filter.lib").smooth(s);
 };
--- trunk/src/plugins/generated/aclipper.cc
+++ trunk/src/plugins/generated/aclipper.cc
@@ -15,31 +15,40 @@ private:
 	double 	fRec0[2];
 	int 	iConst0;
 	double 	fConst1;
+	FAUSTFLOAT 	fslider1;
+	double 	fRec2[2];
 	double 	fConst2;
 	double 	fConst3;
 	double 	fConst4;
 	double 	fConst5;
 	double 	fConst6;
-	double 	fRec3[3];
 	double 	fConst7;
 	double 	fConst8;
-	double 	fVec0[2];
 	double 	fConst9;
 	double 	fConst10;
 	double 	fConst11;
-	FAUSTFLOAT 	fslider1;
 	double 	fConst12;
+	double 	fRec5[3];
 	double 	fConst13;
 	double 	fConst14;
+	double 	fVec0[2];
 	double 	fConst15;
 	double 	fConst16;
-	double 	fRec4[2];
-	double 	fRec2[2];
 	double 	fConst17;
 	double 	fConst18;
+	double 	fConst19;
+	double 	fConst20;
+	double 	fConst21;
+	double 	fConst22;
+	double 	fRec6[2];
+	double 	fRec4[2];
+	double 	fConst23;
+	double 	fConst24;
+	double 	fRec3[3];
+	double 	fConst25;
 	double 	fRec1[2];
 	FAUSTFLOAT 	fslider2;
-	double 	fRec5[2];
+	double 	fRec7[2];
 	void clear_state_f();
 	int load_ui_f(const UiBuilder& b, int form);
 	void init(unsigned int samplingFreq);
@@ -85,12 +94,14 @@ Dsp::~Dsp() {
 inline void Dsp::clear_state_f()
 {
 	for (int i=0; i<2; i++) fRec0[i] = 0;
-	for (int i=0; i<3; i++) fRec3[i] = 0;
-	for (int i=0; i<2; i++) fVec0[i] = 0;
-	for (int i=0; i<2; i++) fRec4[i] = 0;
 	for (int i=0; i<2; i++) fRec2[i] = 0;
+	for (int i=0; i<3; i++) fRec5[i] = 0;
+	for (int i=0; i<2; i++) fVec0[i] = 0;
+	for (int i=0; i<2; i++) fRec6[i] = 0;
+	for (int i=0; i<2; i++) fRec4[i] = 0;
+	for (int i=0; i<3; i++) fRec3[i] = 0;
 	for (int i=0; i<2; i++) fRec1[i] = 0;
-	for (int i=0; i<2; i++) fRec5[i] = 0;
+	for (int i=0; i<2; i++) fRec7[i] = 0;
 }
 
 void Dsp::clear_state_f_static(PluginDef *p)
@@ -103,23 +114,30 @@ inline void Dsp::init(unsigned int samplingFreq)
 	fSamplingFreq = samplingFreq;
 	iConst0 = min(192000, max(1, fSamplingFreq));
 	fConst1 = double(iConst0);
-	fConst2 = (3.64865447573811e-11 * fConst1);
-	fConst3 = (0.000414619826788421 + (fConst1 * (fConst2 - 1.9073341271921e-05)));
-	fConst4 = (0.000829239653576842 - (7.29730895147621e-11 * faustpower<2>(fConst1)));
-	fConst5 = (0.000414619826788421 + (fConst1 * (1.9073341271921e-05 + fConst2)));
-	fConst6 = (1.0 / fConst5);
-	fConst7 = (1.82432723786905e-05 * fConst1);
-	fConst8 = (0 - fConst7);
-	fConst9 = (2.08332871602678e-05 * fConst1);
-	fConst10 = (2.21630714470934e-06 + fConst9);
-	fConst11 = ((2.21630714470934e-06 - fConst9) / fConst10);
-	fConst12 = (9.4e-08 * iConst0);
-	fConst13 = (0.00044179999999999995 * iConst0);
-	fConst14 = (1 + fConst13);
-	fConst15 = (1.0 / (fConst14 * fConst5));
-	fConst16 = (0 - ((1 - fConst13) / fConst14));
-	fConst17 = (0 - fConst9);
-	fConst18 = (1.0 / fConst10);
+	fConst2 = (4.3384046341364e-10 * fConst1);
+	fConst3 = (3.41041934946762e-08 + (fConst1 * (fConst2 - 8.99037897457717e-09)));
+	fConst4 = faustpower<2>(fConst1);
+	fConst5 = (6.82083869893523e-08 - (8.6768092682728e-10 * fConst4));
+	fConst6 = (3.41041934946762e-08 + (fConst1 * (8.99037897457717e-09 + fConst2)));
+	fConst7 = (1.0 / fConst6);
+	fConst8 = (3.64865447573811e-11 * fConst1);
+	fConst9 = (0.000414619826788421 + (fConst1 * (fConst8 - 1.9073341271921e-05)));
+	fConst10 = (0.000829239653576842 - (7.29730895147621e-11 * fConst4));
+	fConst11 = (0.000414619826788421 + (fConst1 * (1.9073341271921e-05 + fConst8)));
+	fConst12 = (1.0 / fConst11);
+	fConst13 = (1.82432723786905e-05 * fConst1);
+	fConst14 = (0 - fConst13);
+	fConst15 = (2.08332871602678e-05 * fConst1);
+	fConst16 = (2.21630714470934e-06 + fConst15);
+	fConst17 = ((2.21630714470934e-06 - fConst15) / fConst16);
+	fConst18 = (9.4e-08 * iConst0);
+	fConst19 = (0.00044179999999999995 * iConst0);
+	fConst20 = (1 + fConst19);
+	fConst21 = (1.0 / (fConst20 * fConst11));
+	fConst22 = (0 - ((1 - fConst19) / fConst20));
+	fConst23 = (0 - fConst15);
+	fConst24 = (1.0 / fConst16);
+	fConst25 = (fConst4 / fConst6);
 	clear_state_f();
 }
 
@@ -131,32 +149,35 @@ void Dsp::init_static(unsigned int samplingFreq, PluginDef *p)
 void always_inline Dsp::compute(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0)
 {
 	double 	fSlow0 = (0.007000000000000006 * (1.0 - double(fslider0)));
-	double 	fSlow1 = double(fslider1);
-	double 	fSlow2 = (fConst12 * ((100000 * fSlow1) + 5700));
-	double 	fSlow3 = (1 - fSlow2);
-	double 	fSlow4 = (1 + fSlow2);
-	double 	fSlow5 = (fConst18 * (1.0 + max(0.0, (fSlow1 - 0.25))));
-	double 	fSlow6 = (0.007000000000000006 * pow(10,(0.05 * double(fslider2))));
+	double 	fSlow1 = (0.007000000000000006 * double(fslider1));
+	double 	fSlow2 = (0.007000000000000006 * pow(10,(0.05 * double(fslider2))));
 	for (int i=0; i<count; i++) {
 		fRec0[0] = (fSlow0 + (0.993 * fRec0[1]));
 		double fTemp0 = (3.91923990498812e-05 * fRec0[0]);
 		double fTemp1 = (0.0593824228028504 + (fConst1 * (3.91923990498812e-05 + (0 - fTemp0))));
-		fRec3[0] = ((double)input0[i] - (fConst6 * ((fConst4 * fRec3[1]) + (fConst3 * fRec3[2]))));
-		double fTemp2 = (((0.000829239653576842 * fRec3[1]) + (fConst7 * fRec3[0])) + (fConst8 * fRec3[2]));
-		fVec0[0] = fTemp2;
-		double fTemp3 = (fConst6 * fVec0[0]);
-		fRec4[0] = ((fConst16 * fRec4[1]) + (fConst15 * ((fSlow4 * fVec0[0]) + (fSlow3 * fVec0[1]))));
-		fRec2[0] = (0 - ((opamp((fRec4[0] - fTemp3)) + (fConst11 * fRec2[1])) - fTemp3));
-		fRec1[0] = (asymhardclip2((fSlow5 * ((fConst17 * fRec2[1]) + (fConst9 * fRec2[0])))) - ((fRec1[1] * (0.0593824228028504 + (fConst1 * (fTemp0 - 3.91923990498812e-05)))) / fTemp1));
-		fRec5[0] = ((0.993 * fRec5[1]) + fSlow6);
-		output0[i] = (FAUSTFLOAT)(0.0593824228028504 * ((fRec5[0] * (fRec1[0] + fRec1[1])) / fTemp1));
+		fRec2[0] = (fSlow1 + (0.993 * fRec2[1]));
+		double fTemp2 = (0.5 + (0.75 * fRec2[0]));
+		double fTemp3 = (0 - (9.26800656732889e-10 * fTemp2));
+		fRec5[0] = ((double)input0[i] - (fConst12 * ((fConst10 * fRec5[1]) + (fConst9 * fRec5[2]))));
+		double fTemp4 = (((0.000829239653576842 * fRec5[1]) + (fConst13 * fRec5[0])) + (fConst14 * fRec5[2]));
+		fVec0[0] = fTemp4;
+		double fTemp5 = (fConst12 * fVec0[0]);
+		double fTemp6 = (fConst18 * (5700 + (100000 * fRec2[0])));
+		fRec6[0] = ((fConst22 * fRec6[1]) + (fConst21 * ((fVec0[0] * (1 + fTemp6)) + (fVec0[1] * (1 - fTemp6)))));
+		fRec4[0] = (0 - ((opamp((fRec6[0] - fTemp5)) + (fConst17 * fRec4[1])) - fTemp5));
+		fRec3[0] = ((fConst24 * ((fConst23 * fRec4[1]) + (fConst15 * fRec4[0]))) - (fConst7 * ((fConst5 * fRec3[1]) + (fConst3 * fRec3[2]))));
+		fRec1[0] = (asymhardclip2((fConst25 * (((fRec3[0] * fTemp3) + (1.85360131346578e-09 * (fRec3[1] * fTemp2))) + (fRec3[2] * fTemp3)))) - ((fRec1[1] * (0.0593824228028504 + (fConst1 * (fTemp0 - 3.91923990498812e-05)))) / fTemp1));
+		fRec7[0] = ((0.993 * fRec7[1]) + fSlow2);
+		output0[i] = (FAUSTFLOAT)(0.0593824228028504 * ((fRec7[0] * (fRec1[0] + fRec1[1])) / fTemp1));
 		// post processing
-		fRec5[1] = fRec5[0];
+		fRec7[1] = fRec7[0];
 		fRec1[1] = fRec1[0];
-		fRec2[1] = fRec2[0];
-		fRec4[1] = fRec4[0];
-		fVec0[1] = fVec0[0];
 		fRec3[2] = fRec3[1]; fRec3[1] = fRec3[0];
+		fRec4[1] = fRec4[0];
+		fRec6[1] = fRec6[0];
+		fVec0[1] = fVec0[0];
+		fRec5[2] = fRec5[1]; fRec5[1] = fRec5[0];
+		fRec2[1] = fRec2[0];
 		fRec0[1] = fRec0[0];
 	}
 }
