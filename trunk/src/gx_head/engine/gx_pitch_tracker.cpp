3cbd311c1353bf5972506896f6137269bd73d64b
Hermann Meyer <brummer-@web.de>
2016-02-28 06:27:39 +0100

    * use fixed samplerate (41000Hz) in Pitchtracker to fix tracking of low frequencys at high samplerate

--- trunk/src/gx_head/engine/gx_pitch_tracker.cpp
+++ trunk/src/gx_head/engine/gx_pitch_tracker.cpp
@@ -53,6 +53,7 @@ PitchTracker::PitchTracker()
       m_pthr(0),
       resamp(),
       m_sampleRate(),
+      fixed_sampleRate(41000),
       m_freq(-1),
       signal_threshold_on(SIGNAL_THRESHOLD_ON),
       signal_threshold_off(SIGNAL_THRESHOLD_OFF),
@@ -113,7 +114,8 @@ bool PitchTracker::setParameters(int priority, int policy, int sampleRate, int b
     if (error) {
         return false;
     }
-    m_sampleRate = sampleRate / DOWNSAMPLE;
+    
+    m_sampleRate = fixed_sampleRate / DOWNSAMPLE;
     resamp.setup(sampleRate, m_sampleRate, 1, 16); // 16 == least quality
 
     if (m_buffersize != buffersize) {
