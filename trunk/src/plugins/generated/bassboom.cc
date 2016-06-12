0cc7bdd986bea3dff76e73631264e12631bf67ec
Hermann Meyer <brummer-@web.de>
2016-06-12 09:45:31 +0200

    * add forgotten file bassboom.cc

---
+++ trunk/src/plugins/generated/bassboom.cc
@@ -0,0 +1,365 @@
+// generated from file '../src/plugins/bassboom.dsp' by dsp2cc:
+// Code generated with Faust 0.9.73 (http://faust.grame.fr)
+
+#include "gx_faust_support.h"
+#include "gx_plugin.h"
+
+namespace pluginlib {
+namespace bassboom {
+
+class Dsp: public PluginDef {
+private:
+	int fSamplingFreq;
+	int 	iVec0[2];
+	double 	fConst0;
+	double 	fConst1;
+	double 	fConst2;
+	double 	fConst3;
+	double 	fConst4;
+	double 	fConst5;
+	double 	fRec2[2];
+	double 	fVec1[2];
+	double 	fConst6;
+	double 	fConst7;
+	double 	fConst8;
+	double 	fRec1[2];
+	double 	fRec0[3];
+	FAUSTFLOAT 	fslider0;
+	double 	fRec3[2];
+	double 	fConst9;
+	double 	fRec5[2];
+	double 	fRec4[3];
+	double 	fConst10;
+	FAUSTFLOAT 	fslider1;
+	double 	fRec6[2];
+	void clear_state_f();
+	int load_ui_f(const UiBuilder& b, int form);
+	static const char *glade_def;
+	void init(unsigned int samplingFreq);
+	void compute(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0);
+	int register_par(const ParamReg& reg);
+
+	static void clear_state_f_static(PluginDef*);
+	static int load_ui_f_static(const UiBuilder& b, int form);
+	static void init_static(unsigned int samplingFreq, PluginDef*);
+	static void compute_static(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0, PluginDef*);
+	static int register_params_static(const ParamReg& reg);
+	static void del_instance(PluginDef *p);
+public:
+	Dsp();
+	~Dsp();
+};
+
+
+
+Dsp::Dsp()
+	: PluginDef() {
+	version = PLUGINDEF_VERSION;
+	flags = 0;
+	id = "bboom";
+	name = N_("Bass Boom");
+	groups = 0;
+	description = N_("Sub Bass Booster"); // description (tooltip)
+	category = N_("Distortion");       // category
+	shortname = N_("B Boom");     // shortname
+	mono_audio = compute_static;
+	stereo_audio = 0;
+	set_samplerate = init_static;
+	activate_plugin = 0;
+	register_params = register_params_static;
+	load_ui = load_ui_f_static;
+	clear_state = clear_state_f_static;
+	delete_instance = del_instance;
+}
+
+Dsp::~Dsp() {
+}
+
+inline void Dsp::clear_state_f()
+{
+	for (int i=0; i<2; i++) iVec0[i] = 0;
+	for (int i=0; i<2; i++) fRec2[i] = 0;
+	for (int i=0; i<2; i++) fVec1[i] = 0;
+	for (int i=0; i<2; i++) fRec1[i] = 0;
+	for (int i=0; i<3; i++) fRec0[i] = 0;
+	for (int i=0; i<2; i++) fRec3[i] = 0;
+	for (int i=0; i<2; i++) fRec5[i] = 0;
+	for (int i=0; i<3; i++) fRec4[i] = 0;
+	for (int i=0; i<2; i++) fRec6[i] = 0;
+}
+
+void Dsp::clear_state_f_static(PluginDef *p)
+{
+	static_cast<Dsp*>(p)->clear_state_f();
+}
+
+inline void Dsp::init(unsigned int samplingFreq)
+{
+	fSamplingFreq = samplingFreq;
+	fConst0 = tan((345.57519189487726 / double(min(192000, max(1, fSamplingFreq)))));
+	fConst1 = (1.0 / faustpower<2>(fConst0));
+	fConst2 = (2 * (1 - fConst1));
+	fConst3 = (1.0 / fConst0);
+	fConst4 = (1 + ((fConst3 - 1.0000000000000004) / fConst0));
+	fConst5 = (1.0 / (1 + ((1.0000000000000004 + fConst3) / fConst0)));
+	fConst6 = (1 + fConst3);
+	fConst7 = (1.0 / fConst6);
+	fConst8 = (0 - ((1 - fConst3) / fConst6));
+	fConst9 = (0 - fConst3);
+	fConst10 = (2 * (0 - fConst1));
+	clear_state_f();
+}
+
+void Dsp::init_static(unsigned int samplingFreq, PluginDef *p)
+{
+	static_cast<Dsp*>(p)->init(samplingFreq);
+}
+
+void always_inline Dsp::compute(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0)
+{
+	double 	fSlow0 = (0.0010000000000000009 * double(fslider0));
+	double 	fSlow1 = (0.0010000000000000009 * pow(10,(0.05 * double(fslider1))));
+	for (int i=0; i<count; i++) {
+		iVec0[0] = 1;
+		fRec2[0] = ((1e-20 * (1 - iVec0[1])) - fRec2[1]);
+		double fTemp0 = ((double)input0[i] + fRec2[0]);
+		fVec1[0] = fTemp0;
+		fRec1[0] = ((fConst8 * fRec1[1]) + (fConst7 * (fVec1[0] + fVec1[1])));
+		fRec0[0] = (fRec1[0] - (fConst5 * ((fConst4 * fRec0[2]) + (fConst2 * fRec0[1]))));
+		fRec3[0] = (fSlow0 + (0.999 * fRec3[1]));
+		double fTemp1 = pow(1e+01,(1.5 * fRec3[0]));
+		double fTemp2 = max((double)-1, min((double)1, (fConst5 * (fTemp1 * (fRec0[2] + (fRec0[0] + (2 * fRec0[1])))))));
+		double fTemp3 = (0 - fTemp2);
+		double fTemp4 = exp((4 * fTemp2));
+		fRec5[0] = ((fConst8 * fRec5[1]) + (fConst7 * ((fConst3 * fVec1[0]) + (fConst9 * fVec1[1]))));
+		fRec4[0] = (fRec5[0] - (fConst5 * ((fConst4 * fRec4[2]) + (fConst2 * fRec4[1]))));
+		fRec6[0] = ((0.999 * fRec6[1]) + fSlow1);
+		output0[i] = (FAUSTFLOAT)(fRec6[0] * ((fConst5 * (((fConst1 * fRec4[0]) + (fConst10 * fRec4[1])) + (fConst1 * fRec4[2]))) + (0.25 * (((fTemp4 - exp((4.8 * fTemp3))) * max(1.0, (0.4 / fTemp1))) / (fTemp4 + exp((4 * fTemp3)))))));
+		// post processing
+		fRec6[1] = fRec6[0];
+		fRec4[2] = fRec4[1]; fRec4[1] = fRec4[0];
+		fRec5[1] = fRec5[0];
+		fRec3[1] = fRec3[0];
+		fRec0[2] = fRec0[1]; fRec0[1] = fRec0[0];
+		fRec1[1] = fRec1[0];
+		fVec1[1] = fVec1[0];
+		fRec2[1] = fRec2[0];
+		iVec0[1] = iVec0[0];
+	}
+}
+
+void __rt_func Dsp::compute_static(int count, FAUSTFLOAT *input0, FAUSTFLOAT *output0, PluginDef *p)
+{
+	static_cast<Dsp*>(p)->compute(count, input0, output0);
+}
+
+int Dsp::register_par(const ParamReg& reg)
+{
+	reg.registerVar("bboom.Drive","","S",N_("Amount of SubBass Harmonics"),&fslider0, 0.33, 0.0, 1.0, 0.01);
+	reg.registerVar("bboom.Gain","","S","",&fslider1, 0.0, -4e+01, 4.0, 0.1);
+	return 0;
+}
+
+int Dsp::register_params_static(const ParamReg& reg)
+{
+	return static_cast<Dsp*>(reg.plugin)->register_par(reg);
+}
+
+const char *Dsp::glade_def = "\
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\
+<interface>\n\
+  <!-- interface-requires gxwidgets 0.0 -->\n\
+  <requires lib=\"gtk+\" version=\"2.20\"/>\n\
+  <!-- interface-naming-policy project-wide -->\n\
+  <object class=\"GtkWindow\" id=\"window1\">\n\
+    <property name=\"can_focus\">False</property>\n\
+    <child>\n\
+      <object class=\"GtkVBox\" id=\"vbox1\">\n\
+        <property name=\"visible\">True</property>\n\
+        <property name=\"can_focus\">False</property>\n\
+        <child>\n\
+          <object class=\"GtkHBox\" id=\"rackbox\">\n\
+            <property name=\"visible\">True</property>\n\
+            <property name=\"can_focus\">False</property>\n\
+            <property name=\"spacing\">4</property>\n\
+            <child>\n\
+              <object class=\"GtkHBox\" id=\"hbox1\">\n\
+                <property name=\"visible\">True</property>\n\
+                <property name=\"can_focus\">False</property>\n\
+                <property name=\"spacing\">10</property>\n\
+                <child>\n\
+                  <object class=\"GtkVBox\" id=\"vbox2\">\n\
+                    <property name=\"visible\">True</property>\n\
+                    <property name=\"can_focus\">False</property>\n\
+                    <child>\n\
+                      <object class=\"GtkLabel\" id=\"label1:rack_label\">\n\
+                        <property name=\"visible\">True</property>\n\
+                        <property name=\"can_focus\">False</property>\n\
+                        <property name=\"label\" translatable=\"yes\">label</property>\n\
+                      </object>\n\
+                      <packing>\n\
+                        <property name=\"expand\">False</property>\n\
+                        <property name=\"fill\">False</property>\n\
+                        <property name=\"position\">0</property>\n\
+                      </packing>\n\
+                    </child>\n\
+                    <child>\n\
+                      <object class=\"GxMidKnob\" id=\"gxbigknob1\">\n\
+                        <property name=\"visible\">True</property>\n\
+                        <property name=\"can_focus\">True</property>\n\
+                        <property name=\"receives_default\">True</property>\n\
+                        <property name=\"var_id\">bboom.Drive</property>\n\
+                        <property name=\"label_ref\">label1:rack_label</property>\n\
+                      </object>\n\
+                      <packing>\n\
+                        <property name=\"expand\">False</property>\n\
+                        <property name=\"fill\">False</property>\n\
+                        <property name=\"position\">1</property>\n\
+                      </packing>\n\
+                    </child>\n\
+                  </object>\n\
+                  <packing>\n\
+                    <property name=\"expand\">False</property>\n\
+                    <property name=\"fill\">False</property>\n\
+                    <property name=\"position\">0</property>\n\
+                  </packing>\n\
+                </child>\n\
+                <child>\n\
+                  <object class=\"GtkVBox\" id=\"vbox3\">\n\
+                    <property name=\"visible\">True</property>\n\
+                    <property name=\"can_focus\">False</property>\n\
+                    <child>\n\
+                      <object class=\"GtkLabel\" id=\"label2:rack_label\">\n\
+                        <property name=\"visible\">True</property>\n\
+                        <property name=\"can_focus\">False</property>\n\
+                        <property name=\"label\" translatable=\"yes\">label</property>\n\
+                      </object>\n\
+                      <packing>\n\
+                        <property name=\"expand\">False</property>\n\
+                        <property name=\"fill\">False</property>\n\
+                        <property name=\"position\">0</property>\n\
+                      </packing>\n\
+                    </child>\n\
+                    <child>\n\
+                      <object class=\"GxSmallKnobR\" id=\"gxbigknob2\">\n\
+                        <property name=\"visible\">True</property>\n\
+                        <property name=\"can_focus\">True</property>\n\
+                        <property name=\"receives_default\">True</property>\n\
+                        <property name=\"var_id\">bboom.Gain</property>\n\
+                        <property name=\"label_ref\">label2:rack_label</property>\n\
+                      </object>\n\
+                      <packing>\n\
+                        <property name=\"expand\">False</property>\n\
+                        <property name=\"fill\">False</property>\n\
+                        <property name=\"position\">1</property>\n\
+                      </packing>\n\
+                    </child>\n\
+                  </object>\n\
+                  <packing>\n\
+                    <property name=\"expand\">False</property>\n\
+                    <property name=\"fill\">False</property>\n\
+                    <property name=\"position\">1</property>\n\
+                  </packing>\n\
+                </child>\n\
+              </object>\n\
+              <packing>\n\
+                <property name=\"expand\">True</property>\n\
+                <property name=\"fill\">False</property>\n\
+                <property name=\"pack_type\">end</property>\n\
+                <property name=\"position\">0</property>\n\
+              </packing>\n\
+            </child>\n\
+          </object>\n\
+          <packing>\n\
+            <property name=\"expand\">True</property>\n\
+            <property name=\"fill\">False</property>\n\
+            <property name=\"position\">0</property>\n\
+          </packing>\n\
+        </child>\n\
+        <child>\n\
+          <object class=\"GtkHBox\" id=\"minibox\">\n\
+            <property name=\"visible\">True</property>\n\
+            <property name=\"can_focus\">False</property>\n\
+            <property name=\"spacing\">4</property>\n\
+            <child>\n\
+              <object class=\"GxHSlider\" id=\"gxhslider1\">\n\
+                <property name=\"visible\">True</property>\n\
+                <property name=\"can_focus\">True</property>\n\
+                <property name=\"receives_default\">True</property>\n\
+                <property name=\"round_digits\">0</property>\n\
+                <property name=\"var_id\">bboom.Drive</property>\n\
+                <property name=\"show_value\">False</property>\n\
+                <property name=\"value_position\">right</property>\n\
+                <property name=\"value_xalign\">0.52000000000000002</property>\n\
+                <property name=\"label_ref\">label0:rack_label</property>\n\
+              </object>\n\
+              <packing>\n\
+                <property name=\"expand\">False</property>\n\
+                <property name=\"fill\">False</property>\n\
+                <property name=\"position\">0</property>\n\
+              </packing>\n\
+            </child>\n\
+            <child>\n\
+              <object class=\"GtkLabel\" id=\"label0:rack_label\">\n\
+                <property name=\"visible\">True</property>\n\
+                <property name=\"can_focus\">False</property>\n\
+                <property name=\"xalign\">0</property>\n\
+                <property name=\"label\" translatable=\"yes\">Level</property>\n\
+              </object>\n\
+              <packing>\n\
+                <property name=\"expand\">False</property>\n\
+                <property name=\"fill\">False</property>\n\
+                <property name=\"position\">1</property>\n\
+              </packing>\n\
+            </child>\n\
+          </object>\n\
+          <packing>\n\
+            <property name=\"expand\">True</property>\n\
+            <property name=\"fill\">True</property>\n\
+            <property name=\"position\">1</property>\n\
+          </packing>\n\
+        </child>\n\
+      </object>\n\
+    </child>\n\
+  </object>\n\
+</interface>\n\
+";
+
+inline int Dsp::load_ui_f(const UiBuilder& b, int form)
+{
+    if (form & UI_FORM_GLADE) {
+        b.load_glade(glade_def);
+        return 0;
+    }
+    if (form & UI_FORM_STACK) {
+#define PARAM(p) ("bboom" "." p)
+
+b.openHorizontalhideBox("");
+    b.create_master_slider(PARAM("Drive"), "Drive");
+b.closeBox();
+b.openHorizontalBox("");
+    b.create_small_rackknobr(PARAM("Drive"), "Drive");
+    b.create_small_rackknobr(PARAM("Gain"), "Gain");
+b.closeBox();
+
+#undef PARAM
+        return 0;
+    }
+	return -1;
+}
+
+int Dsp::load_ui_f_static(const UiBuilder& b, int form)
+{
+	return static_cast<Dsp*>(b.plugin)->load_ui_f(b, form);
+}
+PluginDef *plugin() {
+	return new Dsp();
+}
+
+void Dsp::del_instance(PluginDef *p)
+{
+	delete static_cast<Dsp*>(p);
+}
+
+} // end namespace bassboom
+} // end namespace pluginlib
