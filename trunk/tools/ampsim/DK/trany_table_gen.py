7a307bef6d256c935d7db54d975290c0664e1d6b
Hermann Meyer <brummer-@web.de>
2018-04-17 19:34:30 +0200

    * add filename parameter to trany_table_gen.py

--- trunk/tools/ampsim/DK/trany_table_gen.py
+++ trunk/tools/ampsim/DK/trany_table_gen.py
@@ -138,8 +138,10 @@ double always_inline tranyclip(double x) {
         a[:,0] += self.sig
         return a
 
-    def generate_table(self, p):
+    def generate_table(self, p, filename=None):
         n = 'dkbuild/%s_table.h' % self.model
+        if filename:
+            n = filename
         sys.stdout = open(n, 'w')
         y = p(self.signal())
         c = 0;
