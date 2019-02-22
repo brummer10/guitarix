406015bc35ddb33bf9ebe388538cdf4dfa7f15af
brummer10 <brummer-@web.de>
2019-02-22 16:22:45 +0100

    dkbuilder: allow building of multiple nonlinear tables with build-plug.py

--- trunk/tools/ampsim/DK/gschem-schematics/build-plug.py
+++ trunk/tools/ampsim/DK/gschem-schematics/build-plug.py
@@ -16,7 +16,7 @@ parser.add_argument('-m','--module_id',help='Module ID for plugin [OPTIONAL]', r
 parser.add_argument('-p','--plot',help='frequency plot from the circuit [OPTIONAL]',action="store_true", required=False)
 parser.add_argument('-b','--build',help='build guitarix plugin from the circuit [OPTIONAL]',action="store_true", required=False)
 parser.add_argument('-l','--buildlv2',help='build lv2 plugin from the circuit [OPTIONAL]',action="store_true", required=False)
-parser.add_argument('-t','--table', metavar='N', type=int,help='build nonlinear response table from the N\'t circuit [OPTIONAL]', required=False)
+parser.add_argument('-t','--table', metavar='N', type=int, nargs='+', help='build nonlinear response table from the N\'t circuit [OPTIONAL]', required=False)
 parser.add_argument('-x','--sig_max', metavar='N', type=float, nargs='+', help='max signal send to build the nonlinear response table from the circuit [OPTIONAL]', required=False)
 parser.add_argument('-/','--table_div', metavar='N', type=float, nargs='+', help='divider for nonlinear response table from the circuit [OPTIONAL]', required=False)
 parser.add_argument('-o','--table_op', metavar='N', type=float, nargs='+', help='step operator multiplier for nonlinear response table from the circuit [OPTIONAL]', required=False)
@@ -65,7 +65,7 @@ class FrequencyPlot(object):
         s = c1.make_signal_vector(sig(sig.impulse(), timespan=1))
         c1.stream(s)
         s.plot_spectrum(c1.last_output)
-        ax = pylab.axes()
+        ax = pylab.gca()
         ax.grid()
         ax.yaxis.set_major_formatter(pylab.FormatStrFormatter('%d dB'))
         ax.xaxis.set_major_formatter(pylab.FormatStrFormatter('%d Hz'))
@@ -114,7 +114,7 @@ class Generators(object):
         opts = " " if datatype == "float" else ""
         os.system("%s %s -c -k %s" % (pgm, opts, dspfile))
 
-    def generate_lv2_plugin(self, arg, dspfile, modulename, name=None, nonlin=None):
+    def generate_lv2_plugin(self, arg, dspfile, tablename, modulename, name, nonlin=None):
         if nonlin :
             print ("build nonlin lv2_plugin from: %s" % arg)
         else :
@@ -125,9 +125,10 @@ class Generators(object):
         os.system("%s -p ../%s -n  %s" % (pgm, dspfile, name ))
         # copy table to bundle
         if nonlin :
-            src1 = '../dkbuild/%s_table.h' %  modulename
-            dst1 = 'gx_%s.lv2/dsp/' % modulename
-            copy2(src1, dst1)
+            for a in nonlin:
+                src1 = '../dkbuild/%s_table.h' %  tablename[a-1]
+                dst1 = 'gx_%s.lv2/dsp/' % modulename
+                copy2(src1, dst1)
         os.chdir('gx_%s.lv2' % modulename)
         os.system('make uninstall && make && make install')
         os.chdir(p)
@@ -138,6 +139,7 @@ class DKbuilder(object):
     shortname = args.shortname
     description = args.description
     category = args.category
+    tablename = {}
 
     if name:
         modulename = name.lower()
@@ -145,9 +147,13 @@ class DKbuilder(object):
         schema = args.input[0]
         modulename = schema.split('.')[0].lower()
 
+    def index_exists(self,ls, i):
+        return (0 <= i < len(ls)) or (-len(ls) <= i < 0)
+
     def build(self):
 
         a = 0
+        b = 0
         fdata = ""
         fuidata = ""
 
@@ -157,9 +163,9 @@ class DKbuilder(object):
         # generate faust code and nonlin table
         for sch in args.input:
             a +=1
-            schema = args.input[a-1]
             print ("\nInput file %s: %s" % (a, args.input[a-1]))
 
+            schema = args.input[a-1]
             workfile="gschem-schematics/"+schema
             path = "tmp"
 
@@ -204,28 +210,42 @@ class DKbuilder(object):
                     faustdsp, faustui = c1.get_faust_code(filename=str(dspname))
                 else :
                     faustdsp, faustui = c1.get_simple_faust_code(filename=str(dspname))
-                if args.table == a:
-                    g.generate_nonlin_table(c1, self.modulename, args.sig_max, args.table_div, args.table_op)
-                    src = 'dkbuild/%s_table.h' %  self.modulename
+                if a in args.table:
+                    self.tablename[a-1] = schema.split('.')[0].lower()
+                    b += 1
+                    if args.sig_max and self.index_exists(args.sig_max,b-1) :
+                        m = args.sig_max[b-1]
+                    else :
+                        m = None
+                    if args.table_div and self.index_exists(args.table_div,b-1):
+                        d = args.table_div[b-1]
+                    else:
+                        d = None
+                    if args.table_op and self.index_exists(args.table_op,b-1):
+                        o = args.table_op[b-1]
+                    else:
+                        o = None
+                    g.generate_nonlin_table(c1, self.tablename[a-1], m, d, o)
+                    src = 'dkbuild/%s_table.h' %  self.tablename[a-1]
                     # copy table to build dir
                     copy2(src, dst)
                     # include table use in faust code
-                    faustdsp = faustdsp.replace('with', ': clip with' )
-                    faustdsp +=  '\n    clip = ffunction(float circclip(float), "%s_table.h", "");\n' % self.modulename
+                    faustdsp = faustdsp.replace('with', ': %s_clip with' ) % self.tablename[a-1]
+                    faustdsp +=  '\n    {st}_clip = ffunction(float {st}clip(float), "{st}_table.h", "");\n' .format(st=self.tablename[a-1])
 
                 faustdsp = faustdsp.replace('process', "p%s" % a)
                 fdata += faustdsp
                 fuidata += faustui
 
         # create a guitarix module
-        if args.build or (not args.plot and not args.buildlv2) :
+        if args.build or (not args.table and not args.plot and not args.buildlv2) :
             g.write_final_file(a,dspfile,fdata,dspfileui,fuidata)
             g.generate_gx_plugin(args.input, dspfile, args.table)
 
         # create a LV2 module
         elif args.buildlv2 :
             g.write_final_file(a,dspfile,fdata,dspfileui,fuidata)
-            g.generate_lv2_plugin(args.input, dspfile, self.modulename, self.name, args.table)
+            g.generate_lv2_plugin(args.input, dspfile, self.tablename, self.modulename, self.name, args.table)
 
 def main(argv):
     dk = DKbuilder()
