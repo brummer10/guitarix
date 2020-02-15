1ae4f9efa67b9964fe3bbd8b8a267808d9ca8e8b
Andreas Degert <ad@papyrus-gmbh.de>
2020-02-15 18:23:38 +0100

    SCCS update
     - minimal.scss (new): only basic and important layout settings
     - gx_head_Guitarix.scss: includes minimal.scss, work in progress
     - wscript:
       - produces expanded css in debug mode, and compressed css for
         deploy versions
       - shorter now because some parts moved to waftools/sassc.py
     - sassc.py:
       - new feature "generate_symlink_icons", which scans for the
         "ICON THEME" sections and copies and links the icons
       - detects dependencies on scss imported files

--- trunk/waftools/sassc.py
+++ trunk/waftools/sassc.py
@@ -1,5 +1,44 @@
 from waflib import Task, Utils
-from waflib.TaskGen import extension
+from waflib.TaskGen import extension, feature
+import os, re
+
+def scan_scss(fname):
+    defn = re.compile(' *\* *([-a-zA-Z0-9_]+) *: *([-a-zA-Z0-9_.]+)').match
+    with open(fname) as fd:
+        for line in fd:
+            if line.strip() == '/* ICON THEME':
+                break
+        else:
+            raise ValueError("icon table start not found: '/* ICON THEME'")
+        for line in fd:
+            m = defn(line)
+            if not m:
+                if line.strip() == ('* ICON THEME'):
+                    break
+                continue
+            yield m.group(1, 2)
+        else:
+            raise ValueError("icon table end not found: ' * ICON THEME'")
+
+def link_icon(self):
+    dst = self.link_dest
+    lpath = self.outputs[0].abspath()
+    if os.path.islink(lpath):
+        if os.readlink(lpath) == dst:
+            return
+        os.remove(lpath)
+    elif os.path.exists(lpath):
+        os.remove(lpath)
+    os.symlink(dst, lpath)
+
+Task.task_factory(
+    name    = 'link-icon',
+    func    = link_icon,
+    )
+
+@feature('generate_symlink_icons')
+def add_install_icons(self):
+    pass
 
 @extension('.scss')
 def scss_file(self, node):
@@ -11,6 +50,20 @@ def scss_file(self, node):
         self.install_task = self.add_install_files(
             install_to=self.install_path, install_from=tsk.outputs,
             chmod=getattr(self,'chmod',Utils.O644))
+    if 'generate_symlink_icons' not in self.features:
+        return
+    theme = re.match('gx_head_([^.]*).scss', node.name).group(1)
+    dest = o.parent.find_or_declare(theme)
+    for src, dst in scan_scss(node.abspath()):
+        src += os.path.splitext(dst)[1]
+        lpath = dest.find_or_declare(src)
+        lnk_tgt = os.path.join('..', dst)
+        lnk = self.create_task('link-icon', link_dest = lnk_tgt, keyword = lambda: 'Symlinking')
+        lnk.set_inputs(o.parent.find_resource(dst))
+        lnk.set_outputs(lpath)
+        if getattr(self, 'install_path', None):
+            to = os.path.join(self.install_path, theme, src)
+            self.add_symlink_as(install_to=to, install_from=lnk_tgt)
 
 def scss2css(task):
     src = task.inputs[0].bldpath()
@@ -19,8 +72,8 @@ def scss2css(task):
     p = o.parent
     #gendir = p.parent.get_src().find_dir(p.name+task.gen_dir_suffix)
     #cpy = gendir.make_node(o.name).abspath()
-    task.proc_args = []
-    lst = task.env['SASSC'] + task.proc_args + [src, dst]
+    proc_args = Utils.to_list(getattr(task.generator, 'proc_args', ''))
+    lst = task.env['SASSC'] + proc_args + [src, dst]
     ret = task.exec_command(lst, shell=False)
     return ret
     if ret != 0:
@@ -32,8 +85,29 @@ def scss2css(task):
         return e.errno
     return 0
 
-def scan_scss(task):
-    return [], []
+def scan_scss_imports(task):
+    is_import = re.compile(r"\s*@import\s+([^;]+?)\s*;").match
+    found = []
+    not_found = []
+    def get_fname(p, v):
+        v = v.strip(" '")
+        for suffix in '', '.scss', '.css':
+            for prefix in '', '_':
+                f = prefix + v + suffix
+                f = p.find_resource(f)
+                if f:
+                    found.append(f)
+                    return
+        not_found.append(v)
+    for f in task.inputs:
+        p = f.parent
+        with open(f.abspath()) as fd:
+            for line in fd:
+                m = is_import(line)
+                if m:
+                    for v in m.group(1).split(','):
+                        get_fname(p, v)
+    return found, not_found
 
 Task.task_factory(
     name    = 'scss',
@@ -41,7 +115,7 @@ Task.task_factory(
     color   = 'BLUE',
     ext_in  = '.scss',
     ext_out = '.css',
-    ).scan = scan_scss
+    ).scan = scan_scss_imports
 
 def configure(conf):
     conf.find_program("sassc", var='SASSC', mandatory=True)
