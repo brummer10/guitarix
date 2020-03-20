3a16c882e8e17a028f619897e5b198d6b67e2249
Andreas Degert <ad@papyrus-gmbh.de>
2020-03-20 22:24:21 +0100

    midi connector window: font size
     - replace deprecated gtk_widget_override_font
    
     + signed / unsigned warning in gx_engine_audio.cpp

--- trunk/src/gx_head/engine/gx_engine_audio.cpp
+++ trunk/src/gx_head/engine/gx_engine_audio.cpp
@@ -428,7 +428,7 @@ ModuleSelectorFromList::ModuleSelectorFromList(
 }
 
 ModuleSelectorFromList::~ModuleSelectorFromList() {
-    for (int i = 0; i < size; i++) {
+    for (unsigned int i = 0; i < size; i++) {
         modules[i]->delete_instance(modules[i]);
     }
     delete[] modules;
