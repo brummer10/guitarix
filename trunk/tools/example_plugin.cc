dd518d5eb27e5e816193a5f34f49c74abbd94971
Andreas Degert <ad@papyrus-gmbh.de>
2020-04-19 10:55:51 +0200

    tools/example_plugin.cc: simpler expamle processing

--- trunk/tools/example_plugin.cc
+++ trunk/tools/example_plugin.cc
@@ -16,7 +16,6 @@ private:
     static void del_instance(PluginDef *plugin);
     //
     float level;
-    float x;
 public:
     Dsp();
 };
@@ -34,15 +33,13 @@ Dsp::Dsp()
 }
 
 void Dsp::init(unsigned int samplingFreq, PluginDef *plugin) {
-    Dsp& self = *static_cast<Dsp*>(plugin);
-    self.x = 0;
+    //Dsp& self = *static_cast<Dsp*>(plugin);
 }
 
 void Dsp::process(int count, float *input, float *output, PluginDef *plugin) {
     Dsp& self = *static_cast<Dsp*>(plugin);
     for (int i = 0; i < count; ++i) {
-	output[i] = (input[i] - self.x) * self.level;
-	self.x = input[i];
+        output[i] = input[i] * self.level;
     }
 }
 
@@ -57,10 +54,10 @@ int Dsp::registerparam(const ParamReg& reg) {
 int Dsp::uiloader(const UiBuilder& b, int format) {
     //Dsp& self = *static_cast<Dsp*>(b.plugin);
     if (format & UI_FORM_STACK) {
-	b.openHorizontalhideBox("");
-	b.closeBox();
-	b.create_small_rackknob(PARAM("level"),0);
-	return 0;
+        b.openHorizontalhideBox("");
+        b.closeBox();
+        b.create_small_rackknob(PARAM("level"),0);
+        return 0;
     }
     return -1;
 }
@@ -74,7 +71,7 @@ get_gx_plugin(unsigned int idx, PluginDef **pplugin)
 {
     const int count = 1;
     if (!pplugin) {
-	return count;
+        return count;
     }
     switch (idx) {
     case 0: *pplugin = new Dsp(); return count;
