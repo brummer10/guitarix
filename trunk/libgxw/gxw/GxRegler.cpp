b0b377c296d7ab3f4ed7f3e2c2fbfe181f31efbf
Andreas Degert <ad@papyrus-gmbh.de>
2020-04-22 18:17:01 +0200

    GxRegler.cpp: mv generated source into new file regler_marshal.cc

--- trunk/libgxw/gxw/GxRegler.cpp
+++ trunk/libgxw/gxw/GxRegler.cpp
@@ -25,6 +25,9 @@
 #include "GxControlParameter.h"
 #include <gdk/gdkkeysyms.h>
 
+#include "regler_marshal.cc"
+
+
 #define P_(s) (s)   // FIXME -> gettext
 #define I_(s) (s)   // FIXME -> gettext
 
@@ -151,149 +154,6 @@ static gboolean single_string_accumulator(
 	continue_emission = str == NULL;
 	return continue_emission;
 }
-
-/*
-** marshalers generated with
-** echo -e 'STRING:DOUBLE\nBOOLEAN:BOXED' | \
-**   glib-genmarshal --prefix='marshal' --skip-source --nostdinc --body
-**
-** manually added static to funktion definitions
-** this should be put into a separate source/header file when
-** definitions are used at other places
-*/
-// begin generated marshalers
-
-
-#ifdef G_ENABLE_DEBUG
-#define g_marshal_value_peek_boolean(v)  g_value_get_boolean (v)
-#define g_marshal_value_peek_char(v)     g_value_get_char (v)
-#define g_marshal_value_peek_uchar(v)    g_value_get_uchar (v)
-#define g_marshal_value_peek_int(v)      g_value_get_int (v)
-#define g_marshal_value_peek_uint(v)     g_value_get_uint (v)
-#define g_marshal_value_peek_long(v)     g_value_get_long (v)
-#define g_marshal_value_peek_ulong(v)    g_value_get_ulong (v)
-#define g_marshal_value_peek_int64(v)    g_value_get_int64 (v)
-#define g_marshal_value_peek_uint64(v)   g_value_get_uint64 (v)
-#define g_marshal_value_peek_enum(v)     g_value_get_enum (v)
-#define g_marshal_value_peek_flags(v)    g_value_get_flags (v)
-#define g_marshal_value_peek_float(v)    g_value_get_float (v)
-#define g_marshal_value_peek_double(v)   g_value_get_double (v)
-#define g_marshal_value_peek_string(v)   (char*) g_value_get_string (v)
-#define g_marshal_value_peek_param(v)    g_value_get_param (v)
-#define g_marshal_value_peek_boxed(v)    g_value_get_boxed (v)
-#define g_marshal_value_peek_pointer(v)  g_value_get_pointer (v)
-#define g_marshal_value_peek_object(v)   g_value_get_object (v)
-#define g_marshal_value_peek_variant(v)  g_value_get_variant (v)
-#else /* !G_ENABLE_DEBUG */
-/* WARNING: This code accesses GValues directly, which is UNSUPPORTED API.
- *          Do not access GValues directly in your code. Instead, use the
- *          g_value_get_*() functions
- */
-#define g_marshal_value_peek_boolean(v)  (v)->data[0].v_int
-#define g_marshal_value_peek_char(v)     (v)->data[0].v_int
-#define g_marshal_value_peek_uchar(v)    (v)->data[0].v_uint
-#define g_marshal_value_peek_int(v)      (v)->data[0].v_int
-#define g_marshal_value_peek_uint(v)     (v)->data[0].v_uint
-#define g_marshal_value_peek_long(v)     (v)->data[0].v_long
-#define g_marshal_value_peek_ulong(v)    (v)->data[0].v_ulong
-#define g_marshal_value_peek_int64(v)    (v)->data[0].v_int64
-#define g_marshal_value_peek_uint64(v)   (v)->data[0].v_uint64
-#define g_marshal_value_peek_enum(v)     (v)->data[0].v_long
-#define g_marshal_value_peek_flags(v)    (v)->data[0].v_ulong
-#define g_marshal_value_peek_float(v)    (v)->data[0].v_float
-#define g_marshal_value_peek_double(v)   (v)->data[0].v_double
-#define g_marshal_value_peek_string(v)   (v)->data[0].v_pointer
-#define g_marshal_value_peek_param(v)    (v)->data[0].v_pointer
-#define g_marshal_value_peek_boxed(v)    (v)->data[0].v_pointer
-#define g_marshal_value_peek_pointer(v)  (v)->data[0].v_pointer
-#define g_marshal_value_peek_object(v)   (v)->data[0].v_pointer
-#define g_marshal_value_peek_variant(v)  (v)->data[0].v_pointer
-#endif /* !G_ENABLE_DEBUG */
-
-
-/* STRING:DOUBLE */
-void
-marshal_STRING__DOUBLE (GClosure     *closure,
-                        GValue       *return_value G_GNUC_UNUSED,
-                        guint         n_param_values,
-                        const GValue *param_values,
-                        gpointer      invocation_hint G_GNUC_UNUSED,
-                        gpointer      marshal_data)
-{
-  typedef gchar* (*GMarshalFunc_STRING__DOUBLE) (gpointer     data1,
-                                                 gdouble      arg_1,
-                                                 gpointer     data2);
-  GMarshalFunc_STRING__DOUBLE callback;
-  GCClosure *cc = (GCClosure*) closure;
-  gpointer data1, data2;
-  gchar* v_return;
-
-  g_return_if_fail (return_value != NULL);
-  g_return_if_fail (n_param_values == 2);
-
-  if (G_CCLOSURE_SWAP_DATA (closure))
-    {
-      data1 = closure->data;
-      data2 = g_value_peek_pointer (param_values + 0);
-    }
-  else
-    {
-      data1 = g_value_peek_pointer (param_values + 0);
-      data2 = closure->data;
-    }
-  callback = (GMarshalFunc_STRING__DOUBLE) (marshal_data ? marshal_data : cc->callback);
-
-  v_return = callback (data1,
-                       g_marshal_value_peek_double (param_values + 1),
-                       data2);
-
-  g_value_take_string (return_value, v_return);
-}
-
-
-/* BOOLEAN:BOXED,BOXED */
-void
-marshal_BOOLEAN__BOXED_BOXED (GClosure     *closure,
-                        GValue       *return_value G_GNUC_UNUSED,
-                        guint         n_param_values,
-                        const GValue *param_values,
-                        gpointer      invocation_hint G_GNUC_UNUSED,
-                        gpointer      marshal_data)
-{
-  typedef gboolean (*GMarshalFunc_BOOLEAN__BOXED_BOXED) (gpointer     data1,
-                                                   gpointer     arg_1,
-                                                   gpointer     arg_2,
-                                                   gpointer     data2);
-  GMarshalFunc_BOOLEAN__BOXED_BOXED callback;
-  GCClosure *cc = (GCClosure*) closure;
-  gpointer data1, data2;
-  gboolean v_return;
-
-  g_return_if_fail (return_value != NULL);
-  g_return_if_fail (n_param_values == 3);
-
-  if (G_CCLOSURE_SWAP_DATA (closure))
-    {
-      data1 = closure->data;
-      data2 = g_value_peek_pointer (param_values + 0);
-    }
-  else
-    {
-      data1 = g_value_peek_pointer (param_values + 0);
-      data2 = closure->data;
-    }
-  callback = (GMarshalFunc_BOOLEAN__BOXED_BOXED) (marshal_data ? marshal_data : cc->callback);
-
-  v_return = callback (data1,
-                       g_marshal_value_peek_boxed (param_values + 1),
-                       g_marshal_value_peek_boxed (param_values + 2),
-                       data2);
-
-  g_value_set_boolean (return_value, v_return);
-}
-
-// end generated marshalers
-
 
 #define add_slider_binding(binding_set, keyval, mask, scroll)              \
   gtk_binding_entry_add_signal (binding_set, keyval, mask,                 \
