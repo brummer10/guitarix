7de937d687020fdeb077293d07256807163f0904
brummer10 <brummer-@web.de>
2020-05-05 08:58:12 +0200

    LV2 implement stereo bypass (issue #6)

--- trunk/src/LV2/DSP/gx_bypass.cc
+++ trunk/src/LV2/DSP/gx_bypass.cc
@@ -34,6 +34,8 @@ public:
     inline void init_bypass(uint32_t rate);
     inline bool pre_check_bypass(float *bypass, float *buf, float *input, uint32_t n_samples);
     inline void post_check_bypass(float *buf, float *output, uint32_t n_samples);
+    inline bool pre_check_bypass(float *bypass, float *buf, float *buf1, float *input, float *input1, uint32_t n_samples);
+    inline void post_check_bypass(float *buf, float *buf1, float *output, float *output1, uint32_t n_samples);
     GxBypass();
     ~GxBypass() {};
 };
@@ -52,7 +54,7 @@ inline void GxBypass::init_bypass(uint32_t rate) {
     ramp_down = ramp_down_step;
     ramp_up = 0.0;
 }
-
+//mono
 inline bool GxBypass::pre_check_bypass(float *bypass, float *buf, float *input, uint32_t n_samples) {
 
     if (bypass_ != static_cast<uint32_t>(*(bypass))) {
@@ -71,7 +73,7 @@ inline bool GxBypass::pre_check_bypass(float *bypass, float *buf, float *input, 
     }
     return bypassed;
 }
-
+//mono
 inline void GxBypass::post_check_bypass(float *buf, float *output, uint32_t n_samples) {
 
     // check if ramping is needed
@@ -102,6 +104,70 @@ inline void GxBypass::post_check_bypass(float *buf, float *output, uint32_t n_sa
             }
             fade = min(ramp_up_step,ramp_up) /ramp_up_step ;
             output[i] = output[i] * fade + buf[i] * (1.0 - fade);
+            }
+
+        if (ramp_up >= ramp_up_step) {
+            needs_ramp_up = false;
+            ramp_up = 0.0;
+            ramp_down = ramp_down_step;
+        } else {
+            ramp_down = ramp_up;
+        }
+    }
+}
+// stereo
+inline bool GxBypass::pre_check_bypass(float *bypass, float *buf, float *buf1, float *input, float *input1, uint32_t n_samples) {
+
+    if (bypass_ != static_cast<uint32_t>(*(bypass))) {
+        bypass_ = static_cast<uint32_t>(*(bypass));
+        if (!bypass_) {
+            needs_ramp_down = true;
+            needs_ramp_up = false;
+        } else {
+            needs_ramp_down = false;
+            needs_ramp_up = true;
+            bypassed = false;
+        }
+    }
+    if (needs_ramp_down || needs_ramp_up) {
+        memcpy(buf, input, n_samples*sizeof(float));
+        memcpy(buf1, input1, n_samples*sizeof(float));
+    }
+    return bypassed;
+}
+// stereo
+inline void GxBypass::post_check_bypass(float *buf, float *buf1, float *output, float *output1, uint32_t n_samples) {
+
+    // check if ramping is needed
+    if (needs_ramp_down) {
+        float fade = 0;
+        for (uint32_t i=0; i<n_samples; i++) {
+            if (ramp_down >= 0.0) {
+                --ramp_down; 
+            }
+            fade = max(0.0,ramp_down) /ramp_down_step ;
+            output[i] = output[i] * fade + buf[i] * (1.0 - fade);
+            output1[i] = output1[i] * fade + buf1[i] * (1.0 - fade);
+        }
+
+        if (ramp_down <= 0.0) {
+            needs_ramp_down = false;
+            bypassed = true;
+            ramp_down = ramp_down_step;
+            ramp_up = 0.0;
+        } else {
+            ramp_up = ramp_down;
+        }
+
+    } else if (needs_ramp_up) {
+        float fade = 0;
+        for (uint32_t i=0; i<n_samples; i++) {
+            if (ramp_up < ramp_up_step) {
+                ++ramp_up ;
+            }
+            fade = min(ramp_up_step,ramp_up) /ramp_up_step ;
+            output[i] = output[i] * fade + buf[i] * (1.0 - fade);
+            output1[i] = output1[i] * fade + buf1[i] * (1.0 - fade);
             }
 
         if (ramp_up >= ramp_up_step) {
