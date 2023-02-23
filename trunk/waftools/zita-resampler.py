53560efbbc44a971b7b10290d96b059b67c71eed
brummer10 <brummer-@web.de>
2023-02-23 03:36:50 +0100

    Fix issue #122 guitarix don't compile with zita-resampler 1.10

--- trunk/waftools/zita-resampler.py
+++ trunk/waftools/zita-resampler.py
@@ -1,3 +1,5 @@
+from waflib.Errors import ConfigurationError
+
 def options(opt):
     comp = opt.add_option_group("Additional Library Options")
 
@@ -11,20 +13,23 @@ def configure(conf):
     if conf.options.includeresampler:
         conf.env['HAVE_ZITA_RESAMPLER'] = False
         return
-    expected_zita_resampler_version = 1
-    code="""
-    #include <zita-resampler/resampler.h>
-    #include <zita-resampler/resampler-table.h>
-    #if ZITA_RESAMPLER_MAJOR_VERSION != %d
-    #error
-    #endif
-    int main(){ return 0; }
-    """ % expected_zita_resampler_version
-    conf.check_cxx(
-        fragment=code,
-        lib='zita-resampler',
-        uselib_store='ZITA_RESAMPLER',
-        msg='Checking for zita-resampler >= %d.0' % expected_zita_resampler_version,
-        errmsg="Zita-resampler version %d not found" % expected_zita_resampler_version,
-        mandatory=True,
-        define_name="ZITA_RESAMPLER")
+    try:
+        expected_zita_resampler_version = 1
+        code="""
+        #include <zita-resampler/resampler.h>
+        #include <zita-resampler/resampler-table.h>
+        #if ZITA_RESAMPLER_MAJOR_VERSION != %d
+        #error
+        #endif
+        int main(){ return 0; }
+        """ % expected_zita_resampler_version
+        conf.check_cxx(
+            fragment=code,
+            lib='zita-resampler',
+            uselib_store='ZITA_RESAMPLER',
+            msg='Checking for zita-resampler >= %d.0' % expected_zita_resampler_version,
+            errmsg="Zita-resampler version %d not found" % expected_zita_resampler_version,
+            mandatory=True,
+            define_name="ZITA_RESAMPLER")
+    except ConfigurationError:
+        conf.env['HAVE_ZITA_RESAMPLER'] = False
