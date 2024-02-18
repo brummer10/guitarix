b968bfc493d96d5a1e6bd0bfa39527675e47ea8e
brummer10 <brummer-@web.de>
2024-02-18 12:16:42 +0100

    make submodule check non fatal

--- trunk/waftools/submodule.py
+++ trunk/waftools/submodule.py
@@ -1,15 +1,19 @@
 import re, shutil
 from waflib import Task, Configure, Logs, Options, Context
+from waflib.Errors import WafError, ConfigurationError
 from waflib.TaskGen import extension
 
 def check_submodule(conf):
     opt = conf.options
     env = conf.env
-    s = conf.cmd_and_log("git submodule update --init --recursive", quiet=True)
-    if s:
-        conf.msg(msg='Checking for git submodule',result="Update submodule",color='RED')
-    else:
-        conf.msg(msg='Checking for git submodule',result="Up to Date")
+    try:
+        s = conf.cmd_and_log("git submodule update --init --recursive", quiet=True)
+        if s:
+            conf.msg(msg='Checking for git submodule',result="update submodule",color='RED')
+        else:
+            conf.msg(msg='Checking for git submodule',result="up to date")
+    except WafError:
+        conf.msg(msg='Checking for git submodule',result="not checked",color='YELLOW')
 
 def configure(conf):
     check_submodule(conf)
