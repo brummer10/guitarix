e17ab6cd335fe305eb1c9bc074a2d6128ab9790e
Loïc Bartoletti <lbartoletti@users.noreply.github.com>
2024-10-05 08:29:47 +0200

    fix(xputty.h): Fix build on FreeBSD
    Not only Linux exists and uses xcairo.
    Switch to `#if defined (__linux__) || (__FreeBSD__)` to build also on FreeBSD

--- trunk/src/LV2/xputty/header/xputty.h
+++ trunk/src/LV2/xputty/header/xputty.h
@@ -34,7 +34,7 @@
 #include <math.h>
 #include <cairo.h>
 #include "xputty-mswin.h" // no ifdef (waf dependency check)
-#ifdef __linux__
+#if defined (__linux__) || (__FreeBSD__)
 #include <cairo-xlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
