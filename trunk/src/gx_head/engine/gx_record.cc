73ebd7ea9f74dcad1b186dcb46693061e7e854de
sbailey <sbailey@flidais.bolobot.com>
2025-10-26 20:43:44 +0100

    Adding flac file format

--- trunk/src/gx_head/engine/gx_record.cc
+++ trunk/src/gx_head/engine/gx_record.cc
@@ -97,6 +97,9 @@ inline std::string SCapture::get_ffilename() {
       case(2) :
           name = "guitarix_session0.w64";
           break;
+      case(3) :
+          name = "guitarix_session0.flac";
+          break;
       default :
           break;
     }
@@ -203,6 +206,9 @@ SNDFILE *SCapture::open_stream(std::string fname)
           break;
       case(2) :
           sfinfo.format = SF_FORMAT_W64 | SF_FORMAT_PCM_24;
+          break;
+      case(3) :
+          sfinfo.format = SF_FORMAT_FLAC | SF_FORMAT_PCM_24;
           break;
       default :
           sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_FLOAT;
@@ -367,7 +373,7 @@ void SCapture::compute_static_st(int count, float *input0, float *input1, float 
 
 int SCapture::register_par(const ParamReg& reg)
 {
-    static const value_pair fformat_values[] = {{"wav"},{"ogg"},{"w64"},{0}};
+    static const value_pair fformat_values[] = {{"wav"},{"ogg"},{"w64"},{"flac"},{0}};
     if (channel == 1) {
 	reg.registerFloatVar("recorder.file","","S",N_("select file format"),&fformat, 0.0, 0.0, 2.0, 1.0, fformat_values);
 	reg.registerFloatVar("recorder.rec","","B",N_("Record files to ~/gxrecord/"),&fcheckbox0, 0.0, 0.0, 1.0, 1.0, 0);
